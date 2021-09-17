7b986a2f978e1918ce06b7e3890c30fe04e50a49
Julien Bigot <julien.bigot@cea.fr>
2021-09-17 09:58:34 +0200

    Fix expose_to_pdi

--- include/ddc/pdi.hpp
+++ include/ddc/pdi.hpp
@@ -7,25 +7,37 @@
 
 #include "ddc/block_span.hpp"
 
+template <class ElementType>
+static inline constexpr PDI_inout_t default_access()
+{
+    if constexpr (std::is_const_v<ElementType>) {
+        return PDI_OUT;
+    } else {
+        return PDI_INOUT;
+    }
+};
+
 template <class SupportType, class ElementType, class LayoutStridedPolicy>
 void expose_to_pdi(
         std::string const& name,
-        BlockSpan<SupportType, ElementType, LayoutStridedPolicy> const& data)
+        BlockSpan<SupportType, ElementType, LayoutStridedPolicy> const& data,
+        PDI_inout_t access = default_access<ElementType>())
 {
-    auto dom = data.domain();
-    auto extents = dom.extents().array();
-    PDI_expose((name + "_extents").c_str(), extents.data(), PDI_OUT);
+    auto const& extents = data.domain().extents().array();
+    size_t rank = extents.size();
     if constexpr (std::is_const_v<ElementType>) {
-        PDI_multi_expose(
-                name.c_str(),
-                (name + "_extents").c_str(),
-                extents.data(),
-                PDI_OUT,
-                name.c_str(),
-                const_cast<typename std::remove_const<ElementType>::type*>(data.data()),
-                PDI_OUT,
-                NULL);
-    } else {
-        PDI_expose(name.c_str(), data.data(), PDI_INOUT);
+        assert(!(access & PDI_IN) && "Can not use PDI to input constant data");
     }
+    PDI_multi_expose(
+            name.c_str(),
+            (name + "_rank").c_str(),
+            &rank,
+            PDI_OUT,
+            (name + "_extents").c_str(),
+            const_cast<size_t*>(extents.data()),
+            PDI_OUT,
+            name.c_str(),
+            data.data(),
+            access,
+            NULL);
 }

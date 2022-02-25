4cfd733bf2fa7195be5b5e73fbe33be7f3619c4f
Thomas Padioleau <thomas.padioleau@cea.fr>
2022-02-25 11:50:19 +0100

    Add a new algorithm (#7)
    * Add a new algorithm
    
    * Improve documentation
    
    * Add missing file
    
    * Add some const
    
    * Update test_and_deploy.yml

---
+++ include/ddc/transform_reduce.hpp
@@ -0,0 +1,73 @@
+#pragma once
+
+#include <utility>
+
+#include "ddc/discrete_coordinate.hpp"
+#include "ddc/discrete_domain.hpp"
+#include "ddc/for_each.hpp"
+
+/** A parallel reduction over a 1d domain
+ * @param[in] domain the 1d domain to iterate
+ * @param[in] reduce    a reduction operation
+ * @param[in] transform a functor taking a 1d discrete coordinate as parameter
+ */
+template <class DDim, class T, class BinaryReductionOp, class BinaryTransformOp>
+inline T transform_reduce(
+        serial_policy,
+        DiscreteDomain<DDim> const& domain,
+        T init,
+        BinaryReductionOp&& reduce,
+        BinaryTransformOp&& transform) noexcept
+{
+    DiscreteDomainIterator<DDim> const it_b = domain.begin();
+    DiscreteDomainIterator<DDim> const it_e = domain.end();
+    for (DiscreteDomainIterator<DDim> it = it_b; it != it_e; ++it) {
+        init = reduce(init, transform(*it));
+    }
+    return init;
+}
+
+/** A parallel reduction over a 2d domain
+ * @param[in] domain    the 2d domain to iterate
+ * @param[in] reduce    a reduction operation
+ * @param[in] transform a functor taking a 2d discrete coordinate as parameter
+ */
+template <class DDim1, class DDim2, class T, class BinaryReductionOp, class BinaryTransformOp>
+inline T transform_reduce(
+        serial_policy,
+        DiscreteDomain<DDim1, DDim2> const& domain,
+        T init,
+        BinaryReductionOp&& reduce,
+        BinaryTransformOp&& transform) noexcept
+{
+    DiscreteDomainIterator<DDim1> const it1_b = select<DDim1>(domain).begin();
+    DiscreteDomainIterator<DDim1> const it1_e = select<DDim1>(domain).end();
+    DiscreteDomainIterator<DDim2> const it2_b = select<DDim2>(domain).begin();
+    DiscreteDomainIterator<DDim2> const it2_e = select<DDim2>(domain).end();
+    for (DiscreteDomainIterator<DDim1> it1 = it1_b; it1 != it1_e; ++it1) {
+        for (DiscreteDomainIterator<DDim2> it2 = it2_b; it2 != it2_e; ++it2) {
+            init = reduce(init, transform(DiscreteCoordinate<DDim1, DDim2>(*it1, *it2)));
+        }
+    }
+    return init;
+}
+
+/** A parallel reduction over a nd domain
+ * @param[in] domain    the nd domain to iterate
+ * @param[in] reduce    a reduction operation
+ * @param[in] transform a functor taking a nd discrete coordinate as parameter
+ */
+template <class... DDims, class T, class BinaryReductionOp, class BinaryTransformOp>
+inline T transform_reduce(
+        DiscreteDomain<DDims...> const& domain,
+        T init,
+        BinaryReductionOp&& reduce,
+        BinaryTransformOp&& transform) noexcept
+{
+    return transform_reduce(
+            serial_policy(),
+            domain,
+            init,
+            std::forward<BinaryReductionOp>(reduce),
+            std::forward<BinaryTransformOp>(transform));
+}
---
+++ tests/transform_reduce.cpp
@@ -0,0 +1,50 @@
+#include <ddc/Chunk>
+#include <ddc/transform_reduce>
+
+#include <gtest/gtest.h>
+
+struct DDimX;
+using ElemX = DiscreteCoordinate<DDimX>;
+using DVectX = DiscreteVector<DDimX>;
+using DDomX = DiscreteDomain<DDimX>;
+
+struct DDimY;
+using ElemY = DiscreteCoordinate<DDimY>;
+using DVectY = DiscreteVector<DDimY>;
+using DDomY = DiscreteDomain<DDimY>;
+
+using ElemXY = DiscreteCoordinate<DDimX, DDimY>;
+using DVectXY = DiscreteVector<DDimX, DDimY>;
+using DDomXY = DiscreteDomain<DDimX, DDimY>;
+
+static ElemX constexpr lbound_x(0);
+static DVectX constexpr nelems_x(10);
+
+static ElemY constexpr lbound_y(0);
+static DVectY constexpr nelems_y(12);
+
+static ElemXY constexpr lbound_x_y {lbound_x, lbound_y};
+static DVectXY constexpr nelems_x_y(nelems_x, nelems_y);
+static DDomXY constexpr dom_x_y(lbound_x_y, nelems_x_y);
+
+TEST(TransformReduce, OneDimension)
+{
+    DDomX const dom(lbound_x, nelems_x);
+    Chunk<int, DDomX> chunk(dom);
+    int count = 0;
+    for_each(dom, [&](ElemX const ix) { chunk(ix) = count++; });
+    ASSERT_EQ(
+            transform_reduce(dom, 0, std::plus(), [&](ElemX const ix) { return chunk(ix); }),
+            dom.size() * (dom.size() - 1) / 2);
+}
+
+TEST(TransformReduce, TwoDimensions)
+{
+    DDomXY const dom(lbound_x_y, nelems_x_y);
+    Chunk<int, DDomXY> chunk(dom);
+    int count = 0;
+    for_each(dom, [&](ElemXY const ixy) { chunk(ixy) = count++; });
+    ASSERT_EQ(
+            transform_reduce(dom, 0, std::plus(), [&](ElemXY const ixy) { return chunk(ixy); }),
+            dom.size() * (dom.size() - 1) / 2);
+}

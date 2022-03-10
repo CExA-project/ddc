0b26cfcf9a66f00f6ddb7416d2a0fc4a927af3cd
tpadioleau <tpadioleau@noreply.example.com>
2022-03-10 14:35:49 +0000

    Update to match 39ebd8e16cf81e4c03fc0f7880548fac4b9d518c by push 31

--- navtreeindex0.js
+++ navtreeindex0.js
@@ -225,12 +225,12 @@ var NAVTREEINDEX0 =
 "structDiscreteDomainIterator.html#ac1f220bdad61d5d37dbade5812c69e3e":[3,8,9],
 "structDiscreteDomainIterator.html#ace520a958597ad6905d6bc3d0953087a":[3,8,16],
 "structDiscreteDomainIterator.html#ad3f3d38abe1ec61ba22100f3cf1d7a78":[3,8,17],
-"structis__non__uniform__disretization.html":[3,9],
-"structis__non__uniform__disretization_3_01NonUniformDiscretization_3_01CDim_01_4_01_4.html":[3,10],
+"structis__non__uniform__discretization.html":[3,9],
+"structis__non__uniform__discretization_3_01NonUniformDiscretization_3_01CDim_01_4_01_4.html":[3,10],
 "structis__rectilinear__domain.html":[3,11],
 "structis__rectilinear__domain_3_01DiscreteDomain_3_01DDims_8_8_8_01_4_01_4.html":[3,12],
-"structis__uniform__disretization.html":[3,13],
-"structis__uniform__disretization_3_01UniformDiscretization_3_01CDim_01_4_01_4.html":[3,14],
+"structis__uniform__discretization.html":[3,13],
+"structis__uniform__discretization_3_01UniformDiscretization_3_01CDim_01_4_01_4.html":[3,14],
 "structis__uniform__domain.html":[3,15],
 "structis__uniform__domain_3_01DiscreteDomain_3_01DDims_8_8_8_01_4_01_4.html":[3,16]
 };

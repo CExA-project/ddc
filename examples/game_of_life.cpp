279cafbbc0029637426a407433184e2430f82181
matheo anquetil <ma269972@mdlspc80.extra.cea.fr>
2022-03-14 15:43:22 +0100

    Updated to not use uniform discretization

--- examples/game_of_life.cpp
+++ examples/game_of_life.cpp
@@ -13,11 +13,8 @@
 using cell = bool;
 
 // Name of the axis
-struct X;
-struct Y;
-
-using DDimX = UniformDiscretization<X>;
-using DDimY = UniformDiscretization<Y>;
+struct DDimX;
+struct DDimY;
 
 static unsigned nt = 10;
 static unsigned length = 5;
@@ -25,13 +22,13 @@ static unsigned height = 5;
 
 void blinker_init(DiscreteDomain<DDimX, DDimY> const& domain, Chunk<cell, DiscreteDomain<DDimX, DDimY>>& cells) {
     for_each(domain, [&](DiscreteCoordinate<DDimX, DDimY> const ixy) {
-        double const x = to_real(select<DDimX>(ixy));
-        double const y = to_real(select<DDimY>(ixy));
-        if (y == 2 && ((x-3) * (x-1)<=0)) cells(ixy) = true;
+        DiscreteCoordinate<DDimX> const ix = select<DDimX>(ixy);
+        DiscreteCoordinate<DDimY> const iy = select<DDimY>(ixy);
+        if (iy == DiscreteCoordinate<DDimY>(2) && (ix >= DiscreteCoordinate<DDimX>(1) && ix <= DiscreteCoordinate<DDimX>(3))) 
+            cells(ixy) = true;
         else cells(ixy) = false;
     });
 }
-
 template <class ElementType, class DDimX, class DDimY>
 std::ostream& print_2DChunk(std::ostream& os, DiscreteDomain<DDimX, DDimY> const& domain, Chunk<ElementType, DiscreteDomain<DDimX, DDimY>>& chunk, int length) {
     for_each(domain, [&](DiscreteCoordinate<DDimX, DDimY> const ixy) {
@@ -45,27 +42,6 @@ std::ostream& print_2DChunk(std::ostream& os, DiscreteDomain<DDimX, DDimY> const
 
 int main()
 {
-    // Origin on X
-    Coordinate<X> const min_x(0.);
-
-    // Sampling step on X
-    Coordinate<X> const dx(1);
-
-    // Actual mesh on X
-    init_discretization<DDimX>(min_x, dx);
-
-    // Origin on Y
-    Coordinate<Y> const min_y(0.);
-
-    // Sampling step on Y
-    Coordinate<Y> const dy(1);
-
-    // Actual mesh on Y
-    init_discretization<DDimY>(min_y, dy);
-
-    // Two-dimensional mesh on X,Y
-
-    // Take (nx+2gw) x (ny+2gw) points of `mesh_xy` starting from (0,0)
     DiscreteDomain<DDimX, DDimY> const domain_xy(
             DiscreteVector<DDimX, DDimY>(length, height));
 
@@ -75,12 +51,6 @@ int main()
     // Initialize the whole domain
     blinker_init(domain_xy, cells_in);
     blinker_init(domain_xy, cells_out);
-
-    for_each(domain_xy, [&](DiscreteCoordinate<DDimX, DDimY> const ixy) {
-        DiscreteCoordinate<DDimX> const ix = select<DDimX>(ixy);
-        DiscreteCoordinate<DDimY> const iy = select<DDimY>(ixy);
-        //std::cout << ix << ", " << iy << ": " << cells_in(ix, iy) << "\n";
-    }); std::cout << "\n\n";
 
     std::size_t iter = 0;
     for (; iter < nt; ++iter) {

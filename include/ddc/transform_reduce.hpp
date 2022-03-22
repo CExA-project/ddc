2140c8ed102bc5c705890a267b3a780d07282693
Thomas Padioleau <thomas.padioleau@cea.fr>
2022-03-22 18:01:04 +0100

    Recursive transform_reduce

--- include/ddc/transform_reduce.hpp
+++ include/ddc/transform_reduce.hpp
@@ -8,68 +8,49 @@
 #include "ddc/discrete_domain.hpp"
 #include "ddc/for_each.hpp"
 
-/** A parallel reduction over a 1d domain
- * @param[in] domain the 1d domain to iterate
- * @param[in] reduce    a reduction operation
- * @param[in] transform a functor taking a 1d discrete coordinate as parameter
- */
-template <class DDim, class T, class BinaryReductionOp, class BinaryTransformOp>
-inline T transform_reduce(
-        serial_policy,
-        DiscreteDomain<DDim> const& domain,
+namespace detail {
+
+template <
+        class... DDims,
+        class T,
+        class BinaryReductionOp,
+        class UnaryTransformOp,
+        class... DCoords>
+inline T transform_reduce_serial(
+        DiscreteDomain<DDims...> const& domain,
         T init,
-        BinaryReductionOp&& reduce,
-        BinaryTransformOp&& transform) noexcept
+        BinaryReductionOp const& reduce,
+        UnaryTransformOp const& transform,
+        DCoords const&... dcoords) noexcept
 {
-    DiscreteDomainIterator<DDim> const it_b = domain.begin();
-    DiscreteDomainIterator<DDim> const it_e = domain.end();
-    for (DiscreteDomainIterator<DDim> it = it_b; it != it_e; ++it) {
-        init = reduce(init, transform(*it));
+    if constexpr (sizeof...(DCoords) == sizeof...(DDims)) {
+        return reduce(init, transform(DiscreteCoordinate<DDims...>(dcoords...)));
+    } else {
+        using CurrentDDim = type_seq_element_t<sizeof...(DCoords), detail::TypeSeq<DDims...>>;
+        for (DiscreteCoordinate<CurrentDDim> const ii : select<CurrentDDim>(domain)) {
+            init = transform_reduce_serial(domain, init, reduce, transform, dcoords..., ii);
+        }
+        return init;
     }
-    return init;
 }
 
-/** A parallel reduction over a 2d domain
- * @param[in] domain    the 2d domain to iterate
- * @param[in] reduce    a reduction operation
- * @param[in] transform a functor taking a 2d discrete coordinate as parameter
- */
-template <class DDim1, class DDim2, class T, class BinaryReductionOp, class BinaryTransformOp>
-inline T transform_reduce(
-        serial_policy,
-        DiscreteDomain<DDim1, DDim2> const& domain,
-        T init,
-        BinaryReductionOp&& reduce,
-        BinaryTransformOp&& transform) noexcept
-{
-    DiscreteDomainIterator<DDim1> const it1_b = select<DDim1>(domain).begin();
-    DiscreteDomainIterator<DDim1> const it1_e = select<DDim1>(domain).end();
-    DiscreteDomainIterator<DDim2> const it2_b = select<DDim2>(domain).begin();
-    DiscreteDomainIterator<DDim2> const it2_e = select<DDim2>(domain).end();
-    for (DiscreteDomainIterator<DDim1> it1 = it1_b; it1 != it1_e; ++it1) {
-        for (DiscreteDomainIterator<DDim2> it2 = it2_b; it2 != it2_e; ++it2) {
-            init = reduce(init, transform(DiscreteCoordinate<DDim1, DDim2>(*it1, *it2)));
-        }
-    }
-    return init;
-}
+} // namespace detail
 
 /** A parallel reduction over a nd domain
  * @param[in] domain    the nd domain to iterate
  * @param[in] reduce    a reduction operation
  * @param[in] transform a functor taking a nd discrete coordinate as parameter
  */
-template <class... DDims, class T, class BinaryReductionOp, class BinaryTransformOp>
+template <class... DDims, class T, class BinaryReductionOp, class UnaryTransformOp>
 inline T transform_reduce(
         DiscreteDomain<DDims...> const& domain,
         T init,
         BinaryReductionOp&& reduce,
-        BinaryTransformOp&& transform) noexcept
+        UnaryTransformOp&& transform) noexcept
 {
-    return transform_reduce(
-            serial_policy(),
+    return detail::transform_reduce_serial(
             domain,
             init,
             std::forward<BinaryReductionOp>(reduce),
-            std::forward<BinaryTransformOp>(transform));
+            std::forward<UnaryTransformOp>(transform));
 }

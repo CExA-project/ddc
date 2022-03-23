232d58be3f781beb28994233a09ec1054410c1c6
Thomas Padioleau <thomas.padioleau@cea.fr>
2022-03-23 11:39:58 +0100

    Fix indentation

--- examples/heat_equation.cpp
+++ examples/heat_equation.cpp
@@ -130,8 +130,9 @@ int main()
             x_domain_begin(x_domain.front(), x_post_ghost.extents());
     // our zone at the end of the domain that will be mirrored to the
     // ghost
-    DiscreteDomain const
-            x_domain_end(x_domain.back() - x_pre_ghost.extents() + 1, x_pre_ghost.extents());
+    DiscreteDomain const x_domain_end(
+            x_domain.back() - x_pre_ghost.extents() + 1,
+            x_pre_ghost.extents());
     //! [X-domains]
 
     //! [Y-domains]

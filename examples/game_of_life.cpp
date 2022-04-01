709eec85e1df5c6922a589c0d91db7d9bcc40da5
matheo anquetil <ma269972@mdlspc80.extra.cea.fr>
2022-04-01 13:15:36 +0200

    minor cleaning up

--- examples/game_of_life.cpp
+++ examples/game_of_life.cpp
@@ -29,13 +29,14 @@ void blinker_init(DiscreteDomain<DDimX, DDimY> const& domain, Chunk<cell, Discre
         else cells(ixy) = false;
     });
 }
+
 template <class ElementType, class DDimX, class DDimY>
-std::ostream& print_2DChunk(std::ostream& os, DiscreteDomain<DDimX, DDimY> const& domain, Chunk<ElementType, DiscreteDomain<DDimX, DDimY>>& chunk, int length) {
-    for_each(domain, [&](DiscreteCoordinate<DDimX, DDimY> const ixy) {
-        DiscreteCoordinate<DDimX> const ix = select<DDimX>(ixy);
-        DiscreteCoordinate<DDimY> const iy = select<DDimY>(ixy);
-        os << chunk(iy, ix);
-        if (iy >= length - 1) os << "\n";
+std::ostream& print_2DChunk(std::ostream& os, ChunkSpan<ElementType, DiscreteDomain<DDimX, DDimY>> chunk) {
+    for_each(select<DDimY>(chunk.domain()), [&](DiscreteCoordinate<DDimY> const iy) {
+        for_each(select<DDimX>(chunk.domain()), [&](DiscreteCoordinate<DDimX> const ix) {
+            os << (chunk(ix, iy) ? "*" : ".");
+        });
+        os << "\n";
     });
     return os;
 }
@@ -54,27 +55,33 @@ int main()
 
     std::size_t iter = 0;
     for (; iter < nt; ++iter) {
-        print_2DChunk(std::cout, domain_xy, cells_in, length) << "\n";
-        for_each(domain_xy, [&](DiscreteCoordinate<DDimX, DDimY> const ixy) {
+        print_2DChunk(std::cout, cells_in.span_cview()) << "\n";
+        for_each(cells_in.domain(), [&](DiscreteCoordinate<DDimX, DDimY> const ixy) {
             DiscreteCoordinate<DDimX> const ix = select<DDimX>(ixy);
             DiscreteCoordinate<DDimY> const iy = select<DDimY>(ixy);
             int alive_neighbors = 0;
+            // Iterate on neighbors and increase the count of alive neighbors when necessary
             for (int i = -1; i < 2; ++i) {
                 for (int j = -1; j < 2; j++) {
-                    if (cells_in(ix + i, iy + j) == true) alive_neighbors++;
+                    if (cells_in(ix + i, iy + j)) {
+                        std::cout << "ix : "<< ix << " | i : " << i <<" | ix + i : " << ix + i << "\n";
+                        alive_neighbors++;
+                    }
                 }
             }
-            if (cells_in(ix, iy) == true) {
+            // Update the future status of the current cell depending on its current status and 
+            // its current number of alive neighbors
+            if (cells_in(ixy)) {
                 alive_neighbors--;
-                if (alive_neighbors < 2 || alive_neighbors > 3) cells_out(ix, iy) = false;
+                if (alive_neighbors < 2 || alive_neighbors > 3) cells_out(ixy) = false;
             }
             else {
-                if (alive_neighbors == 3) cells_out(ix, iy) = true;
+                if (alive_neighbors == 3) cells_out(ixy) = true;
             }
         });
         deepcopy(cells_in, cells_out);
     }
-    print_2DChunk(std::cout, domain_xy, cells_in, length) << "\n";
+    print_2DChunk(std::cout, cells_in.span_cview()) << "\n";
 
     return 0;
 }

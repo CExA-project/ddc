9be483835f7d10672a5ab0d2c82163140c19108c
matheo.anquetil@cea.fr <anquetilm@ruche02.cluster>
2022-04-29 15:59:58 +0200

    Linter

--- examples/game_of_life.cpp
+++ examples/game_of_life.cpp
@@ -20,24 +20,37 @@ static unsigned nt = 10;
 static unsigned length = 5;
 static unsigned height = 5;
 
-void blinker_init(DiscreteDomain<DDimX, DDimY> const& domain, Chunk<cell, DiscreteDomain<DDimX, DDimY>>& cells) {
+void blinker_init(
+    DiscreteDomain<DDimX, DDimY> const& domain,
+    Chunk<cell, DiscreteDomain<DDimX, DDimY>>& cells)
+{
     for_each(domain, [&](DiscreteCoordinate<DDimX, DDimY> const ixy) {
         DiscreteCoordinate<DDimX> const ix = select<DDimX>(ixy);
         DiscreteCoordinate<DDimY> const iy = select<DDimY>(ixy);
-        if (iy == DiscreteCoordinate<DDimY>(2) && (ix >= DiscreteCoordinate<DDimX>(1) && ix <= DiscreteCoordinate<DDimX>(3))) 
+        if (iy == DiscreteCoordinate<DDimY>(2)
+            && (ix >= DiscreteCoordinate<DDimX>(1)
+                && ix <= DiscreteCoordinate<DDimX>(3)))
             cells(ixy) = true;
-        else cells(ixy) = false;
+        else
+            cells(ixy) = false;
     });
 }
 
 template <class ElementType, class DDimX, class DDimY>
-std::ostream& print_2DChunk(std::ostream& os, ChunkSpan<ElementType, DiscreteDomain<DDimX, DDimY>> chunk) {
-    for_each(select<DDimY>(chunk.domain()), [&](DiscreteCoordinate<DDimY> const iy) {
-        for_each(select<DDimX>(chunk.domain()), [&](DiscreteCoordinate<DDimX> const ix) {
-            os << (chunk(ix, iy) ? "*" : ".");
-        });
-        os << "\n";
-    });
+std::ostream& print_2DChunk(
+    std::ostream& os,
+    ChunkSpan<ElementType, DiscreteDomain<DDimX, DDimY>> chunk)
+    {
+    for_each(
+            select<DDimY>(chunk.domain()),
+            [&](DiscreteCoordinate<DDimY> const iy) {
+                for_each(
+                        select<DDimX>(chunk.domain()),
+                        [&](DiscreteCoordinate<DDimX> const ix) {
+                os << (chunk(ix, iy) ? "*" : ".");
+                });
+            os << "\n";
+            });
     return os;
 }
 
@@ -56,29 +69,37 @@ int main()
     std::size_t iter = 0;
     for (; iter < nt; ++iter) {
         print_2DChunk(std::cout, cells_in.span_cview()) << "\n";
-        for_each(cells_in.domain(), [&](DiscreteCoordinate<DDimX, DDimY> const ixy) {
-            DiscreteCoordinate<DDimX> const ix = select<DDimX>(ixy);
-            DiscreteCoordinate<DDimY> const iy = select<DDimY>(ixy);
-            int alive_neighbors = 0;
-            // Iterate on neighbors and increase the count of alive neighbors when necessary
-            for (int i = -1; i < 2; ++i) {
-                for (int j = -1; j < 2; j++) {
-                    if (cells_in(ix + i, iy + j)) {
-                        std::cout << "ix : "<< ix << " | i : " << i <<" | ix + i : " << ix + i << "\n";
-                        alive_neighbors++;
+        for_each(
+                cells_in.domain(),
+                [&](DiscreteCoordinate<DDimX, DDimY> const ixy) {
+                    DiscreteCoordinate<DDimX> const ix
+                            = select<DDimX>(ixy);
+                    DiscreteCoordinate<DDimY> const iy
+                            = select<DDimY>(ixy);
+                    int alive_neighbors = 0;
+                    // Iterate on neighbors and increase the count of alive neighbors when necessary
+                    for (int i = -1; i < 2; ++i) {
+                        for (int j = -1; j < 2; j++) {
+                            if (cells_in(ix + i, iy + j)) {
+                                std::cout << "ix : "<< ix
+                                          << " | i : " << i
+                                          <<" | ix + i : " << ix + i
+                                          << "\n";
+                                alive_neighbors++;
+                            }
+                        }
                     }
-                }
-            }
-            // Update the future status of the current cell depending on its current status and 
-            // its current number of alive neighbors
-            if (cells_in(ixy)) {
-                alive_neighbors--;
-                if (alive_neighbors < 2 || alive_neighbors > 3) cells_out(ixy) = false;
-            }
-            else {
-                if (alive_neighbors == 3) cells_out(ixy) = true;
-            }
-        });
+                    // Update the future status of the current cell depending on its current status and 
+                    // its current number of alive neighbors
+                    if (cells_in(ixy)) {
+                        alive_neighbors--;
+                        if (alive_neighbors < 2 || alive_neighbors > 3)
+                            cells_out(ixy) = false;
+                    } else {
+                        if (alive_neighbors == 3)
+                            cells_out(ixy) = true;
+                    }
+                });
         deepcopy(cells_in, cells_out);
     }
     print_2DChunk(std::cout, cells_in.span_cview()) << "\n";

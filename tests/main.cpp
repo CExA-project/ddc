e61612dd45b1b242d878a2f9f2afea65229a9f8b
Julien Bigot <julien.bigot@cea.fr>
2022-06-29 18:28:36 +0200

    Do not init Kokkos if there is no test to run

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,10 +1,23 @@
+// SPDX-License-Identifier: MIT
+
 #include <ddc/ddc.hpp>
 
 #include <gtest/gtest.h>
 
+namespace testing::internal {
+// accessing gtest internals is not very clean, but gtest provides no public access...
+extern bool g_help_flag;
+} // namespace testing::internal
+
 int main(int argc, char** argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
-    ::ScopeGuard scope(argc, argv);
-    return RUN_ALL_TESTS();
+    if (::testing::GTEST_FLAG(list_tests) || ::testing::internal::g_help_flag) {
+        // do not initialize DDC just to list tests or show help so as to be able to run
+        // a Cuda/Hip/etc. enabled DDC with no device available
+        return RUN_ALL_TESTS();
+    } else {
+        ::ScopeGuard scope(argc, argv);
+        return RUN_ALL_TESTS();
+    }
 }

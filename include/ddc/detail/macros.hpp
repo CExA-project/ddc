fd87e0dced62e57092f61d86bfedfa86cdbb58fe
Thomas Padioleau <thomas.padioleau@cea.fr>
2022-10-03 11:54:10 +0200

    Remove diagnostic push/pop, does not seem to have any effect apart from triggering warnings from the host compiler

--- include/ddc/detail/macros.hpp
+++ include/ddc/detail/macros.hpp
@@ -10,12 +10,10 @@
 #define DDC_NV_DIAGNOSTIC_PUSH DDC_MAKE_PRAGMA(nv_diagnostic push)
 #define DDC_NV_DIAGNOSTIC_POP DDC_MAKE_PRAGMA(nv_diagnostic pop)
 #define DDC_NV_DIAG_SUPPRESS(X) DDC_MAKE_PRAGMA(nv_diag_suppress X)
-#define DDC_NV_DIAG_DEFAULT(X) DDC_MAKE_PRAGMA(nv_diag_default X)
 #else
-#define DDC_NV_DIAGNOSTIC_PUSH DDC_MAKE_PRAGMA(diagnostic push)
-#define DDC_NV_DIAGNOSTIC_POP DDC_MAKE_PRAGMA(diagnostic pop)
+#define DDC_NV_DIAGNOSTIC_PUSH
+#define DDC_NV_DIAGNOSTIC_POP
 #define DDC_NV_DIAG_SUPPRESS(X) DDC_MAKE_PRAGMA(diag_suppress X)
-#define DDC_NV_DIAG_DEFAULT(X) DDC_MAKE_PRAGMA(diag_default X)
 #endif
 
 #define DDC_IF_NVCC_THEN_PUSH(X) DDC_NV_DIAGNOSTIC_PUSH

b38e50d8732a07be75df39084d86c75e51c9c239
Thomas Padioleau <thomas.padioleau@cea.fr>
2023-04-25 16:05:35 +0200

    minor improvement

--- tests/uniform_point_sampling.cpp
+++ tests/uniform_point_sampling.cpp
@@ -8,22 +8,20 @@
 
 #include <Kokkos_Core.hpp>
 
+namespace {
+
 struct DimX;
 
 using DDimX = ddc::UniformPointSampling<DimX>;
 
-class UniformPointSamplingTest : public ::testing::Test
-{
-protected:
-    ddc::Coordinate<DimX> origin = ddc::Coordinate<DimX>(-1.);
-    double step = 0.5;
-    ddc::DiscreteVector<DDimX> npoints = ddc::DiscreteVector<DDimX>(5);
-    ddc::DiscreteElement<DDimX> lbound = ddc::DiscreteElement<DDimX>(0);
-    ddc::DiscreteElement<DDimX> point_ix = ddc::DiscreteElement<DDimX>(2);
-    ddc::Coordinate<DimX> point_rx = ddc::Coordinate<DimX>(0.);
-};
+static ddc::Coordinate<DimX> constexpr origin(-1.);
+static double constexpr step = 0.5;
+static ddc::DiscreteElement<DDimX> constexpr point_ix(2);
+static ddc::Coordinate<DimX> constexpr point_rx(0.);
 
-TEST_F(UniformPointSamplingTest, Constructor)
+} // namespace
+
+TEST(UniformPointSamplingTest, Constructor)
 {
     DDimX::Impl<Kokkos::HostSpace> ddim_x(origin, step);
     EXPECT_EQ(ddim_x.origin(), origin);
@@ -33,7 +31,7 @@ TEST_F(UniformPointSamplingTest, Constructor)
 
 TEST(UniformPointSampling, Formatting)
 {
-    DDimX::Impl<Kokkos::HostSpace> ddim_x(ddc::Coordinate<DimX>(-1.), 0.5);
+    DDimX::Impl<Kokkos::HostSpace> ddim_x(origin, step);
     std::stringstream oss;
     oss << ddim_x;
     EXPECT_EQ(oss.str(), "UniformPointSampling( origin=(-1), step=0.5 )");

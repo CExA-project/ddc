4bd5528ad72e609a42db8e0d8cfbc608ba2b56db
Thomas Padioleau <thomas.padioleau@cea.fr>
2023-04-27 15:15:35 +0200

    Add SFINAE over uniform and non uniform point samplings

--- include/ddc/coordinate_fn.hpp
+++ include/ddc/coordinate_fn.hpp
@@ -5,10 +5,17 @@
 #include "ddc/coordinate.hpp"
 #include "ddc/discrete_element.hpp"
 #include "ddc/discrete_space.hpp"
+#include "ddc/non_uniform_point_sampling.hpp"
+#include "ddc/uniform_point_sampling.hpp"
 
 namespace ddc {
 
-template <class... PointSampling>
+template <
+        class... PointSampling,
+        std::enable_if_t<
+                ((is_uniform_sampling_v<
+                          PointSampling> || is_non_uniform_sampling_v<PointSampling>)&&...),
+                int> = 0>
 DDC_INLINE_FUNCTION Coordinate<typename PointSampling::continuous_dimension_type...> coordinate(
         DiscreteElement<PointSampling...> const& c)
 {

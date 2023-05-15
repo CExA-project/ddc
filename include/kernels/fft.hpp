909d9d93c358f23c7da126173f027bbfc61431fe
blegouix <stilynx51@gmail.com>
2023-05-15 14:11:50 +0200

    minor + typo

--- include/kernels/fft.hpp
+++ include/kernels/fft.hpp
@@ -332,7 +332,7 @@ void FFT_core(
     int n[mesh.rank()] = {(int)ddc::get<ddc::UniformPointSampling<X>>(mesh.extents())...};
     int idist = 1;
     int odist = 1;
-    for (size_t i = 0; i < sizeof...(X); i++) {
+    for (int i = 0; i < sizeof...(X); i++) {
         idist = transform_type<Tin, Tout>::value == TransformType::C2R && i == sizeof...(X) - 1
                         ? idist * (n[i] / 2 + 1)
                         : idist * n[i];
@@ -425,7 +425,7 @@ void FFT_core(
     }
 #endif
 #if hipfft_AVAIL && !HIP_FOR_NVIDIA
-    else if constexpr(std::is_same<ExecSpace, Kokkos::HIP::value) {
+    else if constexpr(std::is_same<ExecSpace, Kokkos::HIP>::value) {
         hipStream_t stream = execSpace.hip_stream();
 
         hipfftHandle plan = -1;

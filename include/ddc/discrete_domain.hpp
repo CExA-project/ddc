ab0d51b83c3133fda6615b73eabdd86bae03f89a
Baptiste Legouix <stilynx51@gmail.com>
2023-07-12 15:17:20 +0200

    Experimental templatization gys changes (#147)
    * Coordinate specialization
    
    * change way to get access to CoordinateOf<DiscreteElement>
    
    * cartesian_prod
    
    * nD cartesian_prod
    
    * feedbacks
    
    * Add comments

--- include/ddc/discrete_domain.hpp
+++ include/ddc/discrete_domain.hpp
@@ -365,6 +365,34 @@ using convert_type_seq_to_discrete_domain = typename ConvertTypeSeqToDiscreteDom
 
 } // namespace detail
 
+// Computes the cartesian product of DiscreteDomain types
+// Example usage : "using DDom = cartesian_prod_t<DDom1,DDom2,DDom3>;"
+template <typename... DDom>
+struct cartesian_prod;
+
+template <typename... DDim1, typename... DDim2>
+struct cartesian_prod<ddc::DiscreteDomain<DDim1...>, ddc::DiscreteDomain<DDim2...>>
+{
+    using type = ddc::DiscreteDomain<DDim1..., DDim2...>;
+};
+
+template <typename DDom>
+struct cartesian_prod<DDom>
+{
+    using type = DDom;
+};
+
+template <typename DDom1, typename DDom2, typename... Tail>
+struct cartesian_prod<DDom1, DDom2, Tail...>
+{
+    using type =
+            typename cartesian_prod<typename cartesian_prod<DDom1, DDom2>::type, Tail...>::type;
+};
+
+template <typename... DDom>
+using cartesian_prod_t = typename cartesian_prod<DDom...>::type;
+
+// Computes the substraction DDom_a - DDom_b in the sense of linear spaces(retained dimensions are those in DDom_a which are not in DDom_b)
 template <class... DDimsA, class... DDimsB>
 constexpr auto remove_dims_of(
         DiscreteDomain<DDimsA...> const& DDom_a,

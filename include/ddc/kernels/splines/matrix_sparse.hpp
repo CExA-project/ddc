fea4f15bb037232c2d96b584627b0ed69954ab69
blegouix <stilynx51@gmail.com>
2023-10-12 13:13:53 +0200

    wip on splines-release

--- include/ddc/kernels/splines/matrix_sparse.hpp
+++ include/ddc/kernels/splines/matrix_sparse.hpp
@@ -19,96 +19,6 @@
 #include "matrix.hpp"
 #include "view.hpp"
 
-namespace ddc::detail {
-// TODO : support multiple-rhs case
-// Residual logger (error logged at each iteration)
-template <typename ValueType>
-ValueType get_first_element(const gko::matrix::Dense<ValueType>* norm)
-{
-    return norm->get_executor()->copy_val_to_host(norm->get_const_values());
-}
-
-template <typename ValueType>
-ValueType compute_norm(const gko::matrix::Dense<ValueType>* b)
-{
-    auto exec = b->get_executor();
-    auto b_norm = gko::initialize<gko::matrix::Dense<ValueType>>({0.0}, exec);
-    b->compute_norm2(b_norm);
-    return get_first_element(b_norm.get());
-}
-
-
-template <typename ValueType>
-ValueType compute_residual_norm(
-        const gko::LinOp* system_matrix,
-        const gko::matrix::Dense<ValueType>* b,
-        const gko::matrix::Dense<ValueType>* x)
-{
-    auto exec = system_matrix->get_executor();
-    auto one = gko::initialize<gko::matrix::Dense<ValueType>>({1.0}, exec);
-    auto neg_one = gko::initialize<gko::matrix::Dense<ValueType>>({-1.0}, exec);
-    auto res = gko::clone(b);
-    system_matrix->apply(one, x, neg_one, res);
-    return compute_norm(res.get());
-}
-
-template <typename ValueType>
-struct ResidualLogger : gko::log::Logger
-{
-    void on_iteration_complete(
-            const gko::LinOp*,
-            const gko::size_type&,
-            const gko::LinOp* residual,
-            const gko::LinOp* solution,
-            const gko::LinOp* residual_norm) const override
-    {
-        if (residual_norm) {
-            rec_res_norms.push_back(
-                    get_first_element(gko::as<gko::matrix::Dense<ValueType>>(residual_norm)));
-        } else {
-            rec_res_norms.push_back(compute_norm(gko::as<gko::matrix::Dense<ValueType>>(residual)));
-        }
-        if (solution) {
-            true_res_norms.push_back(compute_residual_norm(
-                    matrix,
-                    b,
-                    gko::as<gko::matrix::Dense<ValueType>>(solution)));
-        } else {
-            true_res_norms.push_back(-1.0);
-        }
-    }
-
-    ResidualLogger(const gko::LinOp* matrix, const gko::matrix::Dense<ValueType>* b)
-        : gko::log::Logger(gko::log::Logger::iteration_complete_mask)
-        , matrix {matrix}
-        , b {b}
-    {
-    }
-
-    void write_data(std::ostream& ostream)
-    {
-        ostream << "Recurrent Residual Norms: " << std::endl;
-        ostream << "[" << std::endl;
-        for (const auto& entry : rec_res_norms) {
-            ostream << " " << entry;
-        }
-        ostream << "];" << std::endl;
-
-        ostream << "True Residual Norms: " << std::endl;
-        ostream << "[" << std::endl;
-        for (const auto& entry : true_res_norms) {
-            ostream << " " << entry;
-        }
-        ostream << "];" << std::endl;
-    }
-
-private:
-    const gko::LinOp* matrix;
-    const gko::matrix::Dense<ValueType>* b;
-    mutable std::vector<ValueType> rec_res_norms;
-    mutable std::vector<ValueType> true_res_norms;
-};
-
 // Matrix class for Csr storage and iterative solve
 template <class ExecSpace>
 class Matrix_Sparse : public Matrix
@@ -158,7 +68,7 @@ public:
     Kokkos::View<double*, Kokkos::HostSpace> data;
 
     int cols_per_par_chunk; // Maximum number of columns of B to be passed to a Ginkgo solver
-    int par_chunks_per_seq_chunk; // Maximum number of teams to be executed in parallel
+    int par_chunks_per_seq_chunk; // Maximum number of Ginkgo calls to be executed in parallel
 
     virtual std::unique_ptr<gko::matrix::Dense<>, std::default_delete<gko::matrix::Dense<>>>
     to_gko_vec(
@@ -259,9 +169,11 @@ public:
         Kokkos::View<double**, Kokkos::LayoutRight, ExecSpace>
                 b_last_buffer("b_last_buffer", n, cols_per_last_par_chunk);
 
+		// Sequential loop
         for (int i = 0; i < n_seq_chunks; i++) {
             int n_par_chunks_in_seq_chunk = i < n_seq_chunks - 1 ? par_chunks_per_seq_chunk
                                                                  : par_chunks_per_last_seq_chunk;
+			// Parallel loop
             Kokkos::parallel_for(
                     Kokkos::RangePolicy<
                             Kokkos::DefaultHostExecutionSpace>(0, n_par_chunks_in_seq_chunk),
@@ -270,7 +182,9 @@ public:
                                 = (i < n_seq_chunks - 1 || j < n_par_chunks_in_seq_chunk - 1)
                                           ? cols_per_par_chunk
                                           : cols_per_last_par_chunk;
+						// Ignore last parallel chunk if empty
                         if (n_equations_in_par_chunk != 0) {
+							// Select window of cols in the current parallel chunk
                             auto par_chunk_window = std::pair<int, int>(
                                     (i * par_chunks_per_seq_chunk + j) * cols_per_par_chunk,
                                     (i * par_chunks_per_seq_chunk + j) * cols_per_par_chunk
@@ -288,6 +202,7 @@ public:
                                                 Kokkos::ALL,
                                                 std::pair<int, int>(0, n_equations_in_par_chunk));
                             }
+							// Copy data window from b to the buffer
                             Kokkos::deep_copy(
                                     b_par_chunk,
                                     Kokkos::subview(b_view, Kokkos::ALL, par_chunk_window));
@@ -303,14 +218,10 @@ public:
                                                    ^ gko::log::Logger::
                                                            polymorphic_object_events_mask,
                                            std::cout);
-                            // gko_exec->add_logger(stream_logger);
                             std::shared_ptr<gko::stop::ResidualNorm<>::Factory> residual_criterion
                                     = gko::stop::ResidualNorm<>::build()
                                               .with_reduction_factor(1e-20)
                                               .on(gko_exec);
-                            std::shared_ptr<const gko::log::Convergence<>> convergence_logger
-                                    = gko::log::Convergence<>::create(gko_exec);
-                            // residual_criterion->add_logger(convergence_logger);
                             auto preconditionner = gko::preconditioner::Jacobi<>::build()
                                                            .with_max_block_size(1u)
                                                            .on(gko_exec);
@@ -326,27 +237,11 @@ public:
                                                   .on(gko_exec);
                             auto solver_ = solver->generate(data_mat_gpu);
                             // solver_->add_logger(stream_logger);
-                            // auto res_logger = std::make_shared<ResidualLogger<double>>(data_mat_gpu.get(), b_vec_batch.get());
-                            // solver_->add_logger(res_logger);
+							
+							// Solve
                             solver_->apply(b_vec_batch, b_vec_batch); // inplace solve
-// res_logger->write_data(std::cout);
 
-// Debug purpose
-#if 0
-      	for (int i=0; i<130; i++) {
-        	// auto b_data = b_vec_batch->get_values();
-        	auto b_data = b_par_chunk.data();
-      		Kokkos::parallel_for(Kokkos::RangePolicy<ExecSpace>(0,1),KOKKOS_LAMBDA (int j) { printf("%f ", b_data[i]); });
-		}
-#endif
-#if 0
-        // Write result
-        std::cout << "-----------------------";
-        write(std::cout, data_mat_gpu);
-        std::cout << "-----------------------";
-        write(std::cout, b_vec_batch);
-
-#endif
+							// Copy the result from the buffer to b
                             Kokkos::deep_copy(
                                     Kokkos::subview(b_view, Kokkos::ALL, par_chunk_window),
                                     b_par_chunk);

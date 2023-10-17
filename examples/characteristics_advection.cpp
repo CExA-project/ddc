8ef8089a79b6f551c15192a18aae45777cc79575
blegouix <stilynx51@gmail.com>
2023-10-17 13:43:08 +0200

    improve advection test

--- examples/characteristics_advection.cpp
+++ examples/characteristics_advection.cpp
@@ -9,9 +9,9 @@
 #include <ddc/ddc.hpp>
 #include <ddc/kernels/splines/bsplines_uniform.hpp>
 #include <ddc/kernels/splines/greville_interpolation_points.hpp>
+#include <ddc/kernels/splines/null_boundary_value.hpp>
 #include <ddc/kernels/splines/spline_builder_batched.hpp>
 #include <ddc/kernels/splines/spline_evaluator_batched.hpp>
-#include <ddc/kernels/splines/null_boundary_value.hpp>
 
 #include <Kokkos_Core.hpp>
 //! [includes]
@@ -21,15 +21,18 @@ static constexpr std::size_t s_degree_x = 3;
 /// Our first continuous dimension
 struct X
 {
-      static constexpr bool PERIODIC = true;
+    static constexpr bool PERIODIC = true;
 };
 //! [X-dimension]
 
 //! [X-discretization]
 /// A uniform discretization of X
 using BSplinesX = ddc::UniformBSplines<X, s_degree_x>;
-
-using DDimX = ddc::GrevilleInterpolationPoints<BSplinesX, ddc::BoundCond::PERIODIC, ddc::BoundCond::PERIODIC>::interpolation_mesh_type;
+using GrevillePoints = ddc::GrevilleInterpolationPoints<
+        BSplinesX,
+        ddc::BoundCond::PERIODIC,
+        ddc::BoundCond::PERIODIC>;
+using DDimX = GrevillePoints::interpolation_mesh_type;
 //! [X-discretization]
 
 //! [Y-space]
@@ -47,33 +50,33 @@ using DDimT = ddc::UniformPointSampling<T>;
 //! [time-space]
 
 //! [display]
-/** A function to pretty print the temperature
+/** A function to pretty print the density 
  * @param time the time at which the output is made
- * @param temp the temperature at this time-step
+ * @param density the density at this time-step
  */
 template <class ChunkType>
-void display(double time, ChunkType temp)
+void display(double time, ChunkType density)
 {
-    double const mean_temp = ddc::transform_reduce(
-                                     temp.domain(),
-                                     0.,
-                                     ddc::reducer::sum<double>(),
-                                     temp)
-                             / temp.domain().size();
+    double const mean_density = ddc::transform_reduce(
+                                        density.domain(),
+                                        0.,
+                                        ddc::reducer::sum<double>(),
+                                        density)
+                                / density.domain().size();
     std::cout << std::fixed << std::setprecision(3);
     std::cout << "At t = " << time << ",\n";
-    std::cout << "  * mean temperature  = " << mean_temp << "\n";
+    std::cout << "  * mean density  = " << mean_density << "\n";
     // take a slice in the middle of the box
-    ddc::ChunkSpan temp_slice
-            = temp[ddc::get_domain<DDimY>(temp).front()
-                   + ddc::get_domain<DDimY>(temp).size() / 2];
-    std::cout << "  * temperature[y:"
-              << ddc::get_domain<DDimY>(temp).size() / 2 << "] = {";
+    ddc::ChunkSpan density_slice = density
+            [ddc::get_domain<DDimY>(density).front()
+             + ddc::get_domain<DDimY>(density).size() / 2];
+    std::cout << "  * density[y:"
+              << ddc::get_domain<DDimY>(density).size() / 2 << "] = {";
     ddc::for_each(
             ddc::policies::serial_host,
-            ddc::get_domain<DDimX>(temp),
+            ddc::get_domain<DDimX>(density),
             [=](ddc::DiscreteElement<DDimX> const ix) {
-                std::cout << std::setw(6) << temp_slice(ix) << " ";
+                std::cout << std::setw(6) << density_slice(ix) << " ";
             });
     std::cout << " }" << std::endl;
 }
@@ -114,23 +117,23 @@ int main(int argc, char** argv)
     std::cout << "Using spectral method \n";
 
     //! [X-global-domain]
-    // Initialization of the global domain in X with gwx ghost points on
-    // each side
-	/*
-    auto const x_domain = ddc::init_discrete_space(
-            DDimX::
-                    init(ddc::Coordinate<X>(x_start),
-                         ddc::Coordinate<X>(x_end),
-                         ddc::DiscreteVector<DDimX>(nb_x_points)));
-						 */
-	ddc::init_discrete_space<BSplinesX>(ddc::Coordinate<X>(x_start),
-                         ddc::Coordinate<X>(x_end),nb_x_points);
-	ddc::init_discrete_space<DDimX>(ddc::GrevilleInterpolationPoints<BSplinesX, ddc::BoundCond::PERIODIC, ddc::BoundCond::PERIODIC>::get_sampling());
+    // Initialization of the global domain in X
+    ddc::init_discrete_space<BSplinesX>(
+            ddc::Coordinate<X>(x_start),
+            ddc::Coordinate<X>(x_end),
+            nb_x_points);
+    ddc::init_discrete_space<DDimX>(
+            ddc::GrevilleInterpolationPoints<
+                    BSplinesX,
+                    ddc::BoundCond::PERIODIC,
+                    ddc::BoundCond::PERIODIC>::get_sampling());
 
+    auto const x_domain = ddc::GrevilleInterpolationPoints<
+            BSplinesX,
+            ddc::BoundCond::PERIODIC,
+            ddc::BoundCond::PERIODIC>::get_domain();
     //! [X-global-domain]
-	auto const x_domain = ddc::GrevilleInterpolationPoints<BSplinesX, ddc::BoundCond::PERIODIC, ddc::BoundCond::PERIODIC>::get_domain(); 
-    // Initialization of the global domain in Y with gwy ghost points on
-    // each side
+    // Initialization of the global domain in Y
     auto const y_domain = ddc::init_discrete_space(
             DDimY::
                     init(ddc::Coordinate<Y>(y_start),
@@ -158,8 +161,7 @@ int main(int argc, char** argv)
                        / (ddc::distance_at_left(iy)
                           * ddc::distance_at_right(iy));
             });
-    ddc::Coordinate<T> const max_dt {
-            0.1}; // Classical stability theory gives .5 but empirically we see that for FFT method we need .2
+    ddc::Coordinate<T> const max_dt {0.1};
 
     // number of time intervals required to reach the end time
     ddc::DiscreteVector<DDimT> const nb_time_steps {
@@ -176,21 +178,21 @@ int main(int argc, char** argv)
     //! [time-domains]
 
     //! [data allocation]
-    // Maps temperature into the full domain (including ghosts) twice:
+    // Maps density into the full domain twice:
     // - once for the last fully computed time-step
-    ddc::Chunk _last_temp(
+    ddc::Chunk _last_density(
             ddc::DiscreteDomain<DDimX, DDimY>(x_domain, y_domain),
             ddc::DeviceAllocator<double>());
 
     // - once for time-step being computed
-    ddc::Chunk _next_temp(
+    ddc::Chunk _next_density(
             ddc::DiscreteDomain<DDimX, DDimY>(x_domain, y_domain),
             ddc::DeviceAllocator<double>());
     //! [data allocation]
 
     //! [initial-conditions]
-    ddc::ChunkSpan const initial_temp = _last_temp.span_view();
-    // Initialize the temperature on the main domain
+    ddc::ChunkSpan const initial_density = _last_density.span_view();
+    // Initialize the density on the main domain
     ddc::DiscreteDomain<DDimX, DDimY> x_mesh
             = ddc::DiscreteDomain<DDimX, DDimY>(x_domain, y_domain);
     ddc::for_each(
@@ -201,27 +203,27 @@ int main(int argc, char** argv)
                         = ddc::coordinate(ddc::select<DDimX>(ixy));
                 double const y
                         = ddc::coordinate(ddc::select<DDimY>(ixy));
-                // initial_temp(ixy) = 9.999 * Kokkos::exp(-(x * x + y * y) / 0.1 / 2);
-                initial_temp(ixy) = 9.999 * ((x * x + y * y) < 0.25);
+                // initial_density(ixy) = 9.999 * Kokkos::exp(-(x * x + y * y) / 0.1 / 2);
+                initial_density(ixy) = 9.999 * ((x * x + y * y) < 0.25);
             });
     //! [initial-conditions]
 
-    ddc::Chunk _host_temp(
+    ddc::Chunk _host_density(
             ddc::DiscreteDomain<DDimX, DDimY>(x_domain, y_domain),
             ddc::HostAllocator<double>());
 
 
     //! [initial output]
     // display the initial data
-    ddc::deepcopy(_host_temp, _last_temp);
+    ddc::deepcopy(_host_density, _last_density);
     display(ddc::coordinate(time_domain.front()),
-            _host_temp[x_domain][y_domain]);
+            _host_density[x_domain][y_domain]);
     // time of the iteration where the last output happened
     ddc::DiscreteElement<DDimT> last_output = time_domain.front();
     //! [initial output]
 
-	// instantiate solver
-	ddc::SplineBuilderBatched<
+    //! [instantiate solver]
+    ddc::SplineBuilderBatched<
             ddc::SplineBuilder<
                     Kokkos::DefaultExecutionSpace,
                     Kokkos::DefaultExecutionSpace::memory_space,
@@ -229,73 +231,102 @@ int main(int argc, char** argv)
                     DDimX,
                     ddc::BoundCond::PERIODIC,
                     ddc::BoundCond::PERIODIC>,
-            DDimX,DDimY>
-            spline_builder(x_mesh);	
-	ddc::SplineEvaluatorBatched<
-            ddc::SplineEvaluator<Kokkos::DefaultExecutionSpace, Kokkos::DefaultExecutionSpace::memory_space, BSplinesX, DDimX>,
-            DDimX, DDimY>
+            DDimX,
+            DDimY>
+            spline_builder(x_mesh);
+    ddc::SplineEvaluatorBatched<
+            ddc::SplineEvaluator<
+                    Kokkos::DefaultExecutionSpace,
+                    Kokkos::DefaultExecutionSpace::memory_space,
+                    BSplinesX,
+                    DDimX>,
+            DDimX,
+            DDimY>
             spline_evaluator(
                     spline_builder.spline_domain(),
                     ddc::g_null_boundary<BSplinesX>,
                     ddc::g_null_boundary<BSplinesX>);
+    //! [instantiate solver]
 
-	// Instantiate chunk of spline coefs to receive output of spline_builder
-    ddc::Chunk coef_alloc(spline_builder.spline_domain(), ddc::KokkosAllocator<double, Kokkos::DefaultExecutionSpace::memory_space>());
+    //! [instantiate intermediate chunks]
+    // Instantiate chunk of spline coefs to receive output of spline_builder
+    ddc::Chunk coef_alloc(
+            spline_builder.spline_domain(),
+            ddc::KokkosAllocator<
+                    double,
+                    Kokkos::DefaultExecutionSpace::memory_space>());
     ddc::ChunkSpan coef = coef_alloc.span_view();
 
-	// Find the coordinates of the characteristics feet
-    ddc::Chunk feet_coords_alloc(spline_builder.vals_domain(), ddc::KokkosAllocator<ddc::Coordinate<X,Y>, Kokkos::DefaultExecutionSpace::memory_space>());
+    // Instantiate chunk to receive feet coords
+    ddc::Chunk feet_coords_alloc(
+            spline_builder.vals_domain(),
+            ddc::KokkosAllocator<
+                    ddc::Coordinate<X, Y>,
+                    Kokkos::DefaultExecutionSpace::memory_space>());
     ddc::ChunkSpan feet_coords = feet_coords_alloc.span_view();
+    //! [instantiate intermediate chunks]
 
 
     //! [time iteration]
     for (auto const iter :
-        time_domain.remove_first(ddc::DiscreteVector<DDimT>(1))) {
+         time_domain.remove_first(ddc::DiscreteVector<DDimT>(1))) {
         //! [time iteration]
 
         //! [boundary conditions]
         //! [boundary conditions]
 
         //! [manipulated views]
-        // a span excluding ghosts of the temperature at the time-step we
+        // a span of the density at the time-step we
         // will build
-        ddc::ChunkSpan const next_temp {_next_temp.span_view()};
-        // a read-only view of the temperature at the previous time-step
-        ddc::ChunkSpan const last_temp {_last_temp.span_view()};
+        ddc::ChunkSpan const next_density {_next_density.span_view()};
+        // a read-only view of the density at the previous time-step
+        ddc::ChunkSpan const last_density {_last_density.span_view()};
         //! [manipulated views]
 
         //! [numerical scheme]
         // Stencil computation on the main domain
-		ddc::for_each(
-		ddc::policies::parallel_device,
-		feet_coords.domain(),
-		DDC_LAMBDA (ddc::DiscreteElement<DDimX,DDimY> const e) {
-		  feet_coords(e) = ddc::Coordinate<X,Y>(ddc::coordinate(ddc::select<DDimX>(e)) - ddc::Coordinate<X>(vx*ddc::step<DDimT>()),ddc::coordinate(ddc::select<DDimY>(e)));
-		});
-		spline_builder(coef,last_temp);
-		spline_evaluator(next_temp,feet_coords.span_cview(),coef.span_cview());
+        // Find the coordinates of the characteristics feet
+        ddc::for_each(
+                ddc::policies::parallel_device,
+                feet_coords.domain(),
+                DDC_LAMBDA(ddc::DiscreteElement<DDimX, DDimY> const e) {
+                    feet_coords(e) = ddc::Coordinate<X, Y>(
+                            ddc::coordinate(ddc::select<DDimX>(e))
+                                    - ddc::Coordinate<X>(
+                                            vx * ddc::step<DDimT>()),
+                            ddc::coordinate(ddc::select<DDimY>(e)));
+                    // Remark : feet_coords could be a chunk of ddc::Coordinate<X> and this line would be replaced by:
+                    // feet_coords(e) = ddc::coordinate(e) - ddc::Coordinate<X>(vx*ddc::step<DDimT>());
+                    // Remark 2 : it is possible to advect along both X and Y directions by performing consecutive basis changes below (requires to define BSplinesY discrete space).
+                });
+        // Interpolate the values at feets on the grid
+        spline_builder(coef, last_density);
+        spline_evaluator(
+                next_density,
+                feet_coords.span_cview(),
+                coef.span_cview());
         //! [numerical scheme]
 
         //! [output]
         if (iter - last_output >= t_output_period) {
             last_output = iter;
-            ddc::deepcopy(_host_temp, _last_temp);
+            ddc::deepcopy(_host_density, _last_density);
             display(ddc::coordinate(iter),
-                    _host_temp[x_domain][y_domain]);
+                    _host_density[x_domain][y_domain]);
         }
         //! [output]
 
         //! [swap]
         // Swap our two buffers
-        std::swap(_last_temp, _next_temp);
+        std::swap(_last_density, _next_density);
         //! [swap]
     }
 
     //! [final output]
     if (last_output < time_domain.back()) {
-        ddc::deepcopy(_host_temp, _last_temp);
+        ddc::deepcopy(_host_density, _last_density);
         display(ddc::coordinate(time_domain.back()),
-                _host_temp[x_domain][y_domain]);
+                _host_density[x_domain][y_domain]);
     }
     //! [final output]
 }

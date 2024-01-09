ee33a6eede869a4a414ce681202a06ba9c76ec22
blegouix <stilynx51@gmail.com>
2024-01-09 14:31:34 +0100

    2D integral

--- tests/splines/batched_2d_spline_builder.cpp
+++ tests/splines/batched_2d_spline_builder.cpp
@@ -552,8 +552,6 @@ static void Batched2dSplineTest()
     ddc::ChunkSpan spline_eval_deriv2 = spline_eval_deriv2_alloc.span_view();
     ddc::Chunk spline_eval_deriv12_alloc(dom_vals, ddc::KokkosAllocator<double, MemorySpace>());
     ddc::ChunkSpan spline_eval_deriv12 = spline_eval_deriv12_alloc.span_view();
-    ddc::Chunk spline_eval_integrals_alloc(dom_batch, ddc::KokkosAllocator<double, MemorySpace>());
-    ddc::ChunkSpan spline_eval_integrals = spline_eval_integrals_alloc.span_view();
 
     // Call spline_evaluator on the same mesh we started with
     spline_evaluator_batched(spline_eval, coords_eval.span_cview(), coef.span_cview());
@@ -563,9 +561,6 @@ static void Batched2dSplineTest()
             .deriv_dim_2(spline_eval_deriv2, coords_eval.span_cview(), coef.span_cview());
     spline_evaluator_batched
             .deriv_1_and_2(spline_eval_deriv12, coords_eval.span_cview(), coef.span_cview());
-    /*
-    spline_evaluator_batched.integrate(spline_eval_integrals, coef.span_cview());
-	*/
 
     // Checking errors (we recover the initial values)
     double max_norm_error = ddc::transform_reduce(
@@ -607,48 +602,15 @@ static void Batched2dSplineTest()
                 return Kokkos::abs(spline_eval_deriv12(e) - evaluator.deriv(x, y, 1, 1));
             });
 
-    ddc::for_each(spline_eval_deriv1.domain(), [=](auto const e) {
-        Coord<I1> const x = ddc::coordinate(ddc::select<IDim<I1, I1, I2>>(e));
-        Coord<I2> const y = ddc::coordinate(ddc::select<IDim<I2, I1, I2>>(e));
-        // std::cout << vals(e) << " ";
-        // std::cout << spline_eval(e) << " - ";
-        // std::cout << evaluator.deriv(x,y,1,0) << " ";
-        // std::cout << spline_eval_deriv1(e) << " - ";
-    });
-    /*
-    double max_norm_error_integ = ddc::transform_reduce(
-            ddc::policies::policy(exec_space),
-            spline_eval_integrals.domain(),
-            0.,
-            ddc::reducer::max<double>(),
-            DDC_LAMBDA(typename decltype(spline_builder)::batch_domain_type::
-                               discrete_element_type const e) {
-                return Kokkos::abs(
-                        spline_eval_integrals(e) - evaluator.deriv(xN<I>(), -1)
-                        + evaluator.deriv(x0<I>(), -1));
-            });
-	*/
-
     double const max_norm = evaluator.max_norm();
     double const max_norm_diff1 = evaluator.max_norm(1, 0);
     double const max_norm_diff2 = evaluator.max_norm(0, 1);
     double const max_norm_diff12 = evaluator.max_norm(1, 1);
-    /*
-    double const max_norm_int = evaluator.max_norm(-1);
-	*/
 
     EXPECT_LE(max_norm_error, 1.0e-14 * max_norm);
     EXPECT_LE(max_norm_error_diff1, 1e-12 * max_norm_diff1);
     EXPECT_LE(max_norm_error_diff2, 1e-12 * max_norm_diff2);
     EXPECT_LE(max_norm_error_diff12, 1e-12 * max_norm_diff12);
-
-    /*
-    EXPECT_LE(
-            max_norm_error_integ,
-            std::
-                    max(error_bounds.error_bound_on_int(dx<I>(ncells), s_degree),
-                        1.0e-14 * max_norm_int));
-	*/
 }
 
 #if defined(BC_PERIODIC) && defined(BSPLINES_TYPE_UNIFORM)

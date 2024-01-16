bf87ed206257c6506d1f9b46e4399ce820dca292
Baptiste Legouix <stilynx51@gmail.com>
2024-01-16 10:12:56 +0100

    Update tests/splines/batched_2d_spline_builder.cpp
    Co-authored-by: Thomas Padioleau <thomas.padioleau@cea.fr>

--- tests/splines/batched_2d_spline_builder.cpp
+++ tests/splines/batched_2d_spline_builder.cpp
@@ -269,7 +269,6 @@ static void Batched2dSplineTest()
     // Compute usefull domains (dom_interpolation, dom_batch, dom_bsplines and dom_spline)
     ddc::DiscreteDomain<IDim<I1, I1, I2>, IDim<I2, I1, I2>> const dom_interpolation
             = spline_builder.interpolation_domain();
-    auto const dom_batch = spline_builder.batch_domain();
     auto const dom_spline = spline_builder.spline_domain();
 
     // Allocate and fill a chunk containing values to be passed as input to spline_builder. Those are values of cosine along interest dimension duplicated along batch dimensions

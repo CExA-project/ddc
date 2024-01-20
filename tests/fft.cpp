a8e73c9b515aa3745b91ab1c88a33318427f4c02
Thomas Padioleau <thomas.padioleau@cea.fr>
2024-01-20 23:31:22 +0100

    Use dedicated algorithm

--- tests/fft.cpp
+++ tests/fft.cpp
@@ -129,11 +129,7 @@ static void test_fft_norm(ddc::FFT_Normalization const norm)
 
     ddc::Chunk f_alloc = ddc::Chunk(x_mesh, ddc::KokkosAllocator<Tin, MemorySpace>());
     ddc::ChunkSpan f = f_alloc.span_view();
-    ddc::for_each(
-            ddc::policies::policy(exec_space),
-            f.domain(),
-            KOKKOS_LAMBDA(DElem<DDim<X>> const e) { f(e) = static_cast<Tin>(1); });
-
+    ddc::fill(f, Tin(1));
 
     ddc::Chunk f_bis_alloc(f.domain(), ddc::KokkosAllocator<Tin, MemorySpace>());
     ddc::ChunkSpan f_bis = f_bis_alloc.span_view();

26aa3d2dd19d6c21a997582ac174e5ce0f18591c
blegouix <stilynx51@gmail.com>
2024-01-21 20:25:48 +0100

    generalized ConstantExtrapolation to nD case

--- include/ddc/kernels/splines/null_extrapolation_rule.hpp
+++ include/ddc/kernels/splines/null_extrapolation_rule.hpp
@@ -5,10 +5,11 @@ struct NullExtrapolationRule
 {
     explicit NullExtrapolationRule() {}
 
-    template <class BSplines, class Layout, class MemorySpace>
+    template <class CoordType, class... BSplines, class Layout, class MemorySpace>
     KOKKOS_FUNCTION double operator()(
-            ddc::Coordinate<typename BSplines::tag_type>,
-            ddc::ChunkSpan<const double, ddc::DiscreteDomain<BSplines>, Layout, MemorySpace>) const
+            CoordType,
+            ddc::ChunkSpan<const double, ddc::DiscreteDomain<BSplines...>, Layout, MemorySpace>)
+            const
     {
         return 0.0;
     }

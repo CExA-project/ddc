56e42671d530e2364b7155e5d8693ae5d28fb916
Thomas Padioleau <thomas.padioleau@cea.fr>
2024-01-21 18:58:52 +0100

    Remove comment

--- tests/discrete_space/discrete_space.cpp
+++ tests/discrete_space/discrete_space.cpp
@@ -13,7 +13,6 @@ void do_not_optimize_away_discrete_space_tests() {}
 
 TEST(DiscreteSpace, Initialization)
 {
-    // ddc::init_discrete_space<QMeshX>(std::vector<ddc::Coordinate<QDimX>>());
     ddc::init_discrete_space(DDimX::
                                      init(ddc::Coordinate<DimX>(0),
                                           ddc::Coordinate<DimX>(1),

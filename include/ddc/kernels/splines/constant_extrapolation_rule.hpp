8039c24f9f0ec2806da90a8dd1ac662745bda64c
blegouix <stilynx51@gmail.com>
2024-01-22 11:21:52 +0100

    wip

--- include/ddc/kernels/splines/constant_extrapolation_rule.hpp
+++ include/ddc/kernels/splines/constant_extrapolation_rule.hpp
@@ -4,14 +4,49 @@
 
 namespace ddc {
 
-template <class DDim>
+template <class... Dim>
 struct ConstantExtrapolationRule
 {
+};
+
+template <class Dim>
+struct ConstantExtrapolationRule<DDim>
+{
 private:
-    ddc::DiscreteElement<DDim> m_eval_pos;
+    ddc::Coordinate<Dim> m_eval_pos;
 
 public:
-    explicit ConstantExtrapolationRule(ddc::DiscreteElement<DDim> eval_pos) : m_eval_pos(eval_pos)
+    explicit ConstantExtrapolationRule(ddc::Coordinate<Dim> eval_pos) : m_eval_pos(eval_pos)
+    {
+    }
+
+    template <class CoordType, class BSplines, class Layout, class MemorySpace>
+    KOKKOS_FUNCTION double operator()(
+            CoordType,
+            ddc::ChunkSpan<double const, ddc::DiscreteDomain<BSplines>, Layout, MemorySpace> const
+                    spline_coef) const
+    {
+        std::array<double, BSplines::degree() + 1> vals;
+
+        ddc::DiscreteElement<BSplines> idx
+                = ddc::discrete_space<BSplines>().eval_basis(vals, m_eval_pos);
+
+        double y = 0.0;
+        for (std::size_t i = 0; i < BSplines::degree() + 1; ++i) {
+            y += spline_coef(idx + i) * vals[i];
+        }
+        return y;
+    }
+};
+
+template <class Dim1, class Dim2>
+struct ConstantExtrapolationRule<Dim1,Dim2>
+{
+private:
+    ddc::Coordinate<Dim1,Dim2> m_eval_pos;
+
+public:
+    explicit ConstantExtrapolationRule(ddc::Coordinate<Dim1,Dim2> eval_pos) : m_eval_pos(eval_pos)
     {
     }
 

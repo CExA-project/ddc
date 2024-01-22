273fce18d1b162df44e14db53028cb7131c8cb39
blegouix <stilynx51@gmail.com>
2024-01-22 09:45:35 +0100

    Revert "generalized ConstantExtrapolation to nD case"
    This reverts commit 26aa3d2dd19d6c21a997582ac174e5ce0f18591c.

--- include/ddc/kernels/splines/constant_extrapolation_rule.hpp
+++ include/ddc/kernels/splines/constant_extrapolation_rule.hpp
@@ -4,73 +4,32 @@
 
 namespace ddc {
 
-namespace detail {
-
-template <class... BSplines, class Layout, class MemorySpace>
-KOKKOS_FUNCTION void ConstantExtrapolationRuleNestedLooper(
-        std::tuple<std::array<double, BSplines::degree() + 1>...> vals,
-        ddc::ChunkSpan<double const, ddc::DiscreteDomain<BSplines...>, Layout, MemorySpace> const
-                spline_coef,
-        std::tuple<ddc::DiscreteElement<BSplines>...> idx,
-        std::tuple<std::conditional_t<true, std::size_t, BSplines>...> sizes,
-        std::tuple<std::conditional_t<true, std::size_t, BSplines>...> iterator,
-        std::size_t index,
-        int& y)
-{
-    if (index == sizeof...(BSplines) - 1) {
-        double tmp = 1;
-        for (std::size_t i = 0; i < sizeof...(BSplines); i++) {
-            tmp *= vals[i][iterator[i]];
-        }
-        std::size_t j = 0;
-        y += tmp
-             * spline_coef(ddc::DiscreteElement<BSplines...>(
-                     (idx[j] + iterator[j++] + 0 * BSplines::degree())...));
-        return;
-    }
-
-    for (iterator[index] = 0; iterator[index] < sizes[index]; iterator[index]++) {
-        ConstantExtrapolationRuleNestedLooper(iterator, index + 1, y);
-    }
-}
-
-} // namespace detail
-
-template <class... Dim>
+template <class DDim>
 struct ConstantExtrapolationRule
 {
 private:
-    ddc::Coordinate<Dim...> m_eval_pos;
+    ddc::DiscreteElement<DDim> m_eval_pos;
 
 public:
-    explicit ConstantExtrapolationRule(ddc::Coordinate<Dim...> eval_pos)
-        : m_eval_pos(eval_pos)
+    explicit ConstantExtrapolationRule(ddc::DiscreteElement<DDim> eval_pos) : m_eval_pos(eval_pos)
     {
     }
 
-    template <class CoordType, class... BSplines, class Layout, class MemorySpace>
+    template <class CoordType, class BSplines, class Layout, class MemorySpace>
     KOKKOS_FUNCTION double operator()(
             CoordType,
-            ddc::ChunkSpan<
-                    double const,
-                    ddc::DiscreteDomain<BSplines...>,
-                    Layout,
-                    MemorySpace> const spline_coef) const
+            ddc::ChunkSpan<double const, ddc::DiscreteDomain<BSplines>, Layout, MemorySpace> const
+                    spline_coef) const
     {
-        std::tuple<std::array<double, BSplines::degree() + 1>...> vals;
-        std::tuple<ddc::DiscreteElement<BSplines>...> idx;
-        std::tuple<std::conditional_t<true, std::size_t, BSplines>...> iterator(
-                (0 * BSplines::degree())...);
-        std::tuple<std::conditional_t<true, std::size_t, BSplines>...> sizes(
-                (BSplines::degree() + 1)...);
+        std::array<double, BSplines::degree() + 1> vals;
 
-        std::size_t i = 0;
-        (idx[i] = ddc::discrete_space<BSplines>().eval_basis(vals[i++], m_eval_pos), ...);
+        ddc::DiscreteElement<BSplines> idx
+                = ddc::discrete_space<BSplines>().eval_basis(vals, m_eval_pos);
 
         double y = 0.0;
-        ddc::detail::
-                ConstantExtrapolationRuleNestedLooper(vals, spline_coef, idx, sizes, iterator, index, y);
-
+        for (std::size_t i = 0; i < BSplines::degree() + 1; ++i) {
+            y += spline_coef(idx + i) * vals[i];
+        }
         return y;
     }
 };
--- include/ddc/kernels/splines/null_extrapolation_rule.hpp
+++ include/ddc/kernels/splines/null_extrapolation_rule.hpp
@@ -5,11 +5,10 @@ struct NullExtrapolationRule
 {
     explicit NullExtrapolationRule() {}
 
-    template <class CoordType, class... BSplines, class Layout, class MemorySpace>
+    template <class BSplines, class Layout, class MemorySpace>
     KOKKOS_FUNCTION double operator()(
-            CoordType,
-            ddc::ChunkSpan<const double, ddc::DiscreteDomain<BSplines...>, Layout, MemorySpace>)
-            const
+            ddc::Coordinate<typename BSplines::tag_type>,
+            ddc::ChunkSpan<const double, ddc::DiscreteDomain<BSplines>, Layout, MemorySpace>) const
     {
         return 0.0;
     }

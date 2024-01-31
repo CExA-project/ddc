37b0f6455e320504e98c247cf2ec57866188b70d
thierry antoun <thierry.antoun@cea.fr>
2024-01-31 17:53:24 +0100

    Work in progress issue #254

--- include/ddc/for_each.hpp
+++ include/ddc/for_each.hpp
@@ -127,54 +127,14 @@ inline void for_each_kokkos(
     }
 }
 
-template <class RetType, class Element, std::size_t N, class Functor, class... Is>
-inline void for_each_serial(
-        std::array<Element, N> const& begin,
-        std::array<Element, N> const& end,
-        Functor const& f,
-        Is const&... is) noexcept
-{
-    static constexpr std::size_t I = sizeof...(Is);
-    if constexpr (I == N) {
-        f(RetType(is...));
-    } else {
-        for (Element ii = begin[I]; ii < end[I]; ++ii) {
-            for_each_serial<RetType>(begin, end, f, is..., ii);
-        }
-    }
-}
-
 } // namespace detail
 
-/// Serial execution on the host
-struct serial_host_policy
-{
-};
-
-/** iterates over a nD domain using the serial execution policy
- * @param[in] domain the domain over which to iterate
- * @param[in] f      a functor taking an index as parameter
- */
-template <class... DDims, class Functor>
-inline void for_each(
-        serial_host_policy,
-        DiscreteDomain<DDims...> const& domain,
-        Functor&& f) noexcept
-{
-    DiscreteElement<DDims...> const ddc_begin = domain.front();
-    DiscreteElement<DDims...> const ddc_end = domain.front() + domain.extents();
-    std::array const begin = detail::array(ddc_begin);
-    std::array const end = detail::array(ddc_end);
-    detail::for_each_serial<DiscreteElement<DDims...>>(begin, end, std::forward<Functor>(f));
-}
-
-/** iterates over a nD extent using the serial execution policy
+/** iterates over a nD extent using the serial execution 
  * @param[in] extent the extent over which to iterate
  * @param[in] f      a functor taking an index as parameter
  */
 template <class... DDims, class Functor>
 inline void for_each_n(
-        serial_host_policy,
         DiscreteVector<DDims...> const& extent,
         Functor&& f) noexcept
 {
@@ -182,103 +142,33 @@ inline void for_each_n(
     DiscreteVector<DDims...> const ddc_end = extent;
     std::array const begin = detail::array(ddc_begin);
     std::array const end = detail::array(ddc_end);
-    detail::for_each_serial<DiscreteVector<DDims...>>(begin, end, std::forward<Functor>(f));
+    detail::for_each_kokkos<Kokkos::Serial, DiscreteVector<DDims...>>(begin, end, std::forward<Functor>(f));
 }
 
-/// Parallel execution on the default device
-struct parallel_host_policy
-{
-};
-
-/** iterates over a nD domain using the serial execution policy
+/** iterates over a nD domain 
  * @param[in] domain the domain over which to iterate
  * @param[in] f      a functor taking an index as parameter
  */
-template <class... DDims, class Functor>
+template <class ExecSpace, class... DDims, class Functor>
 inline void for_each(
-        parallel_host_policy,
         DiscreteDomain<DDims...> const& domain,
         Functor&& f) noexcept
 {
-    detail::for_each_kokkos<Kokkos::DefaultHostExecutionSpace>(domain, std::forward<Functor>(f));
-}
-
-/// Kokkos parallel execution uisng MDRange policy
-struct parallel_device_policy
-{
-};
-
-/** iterates over a nD domain using the parallel_device_policy execution policy
- * @param[in] domain the domain over which to iterate
- * @param[in] f      a functor taking an index as parameter
- */
-template <class... DDims, class Functor>
-inline void for_each(
-        parallel_device_policy,
-        DiscreteDomain<DDims...> const& domain,
-        Functor&& f) noexcept
-{
-    detail::for_each_kokkos<Kokkos::DefaultExecutionSpace>(domain, std::forward<Functor>(f));
-}
-
-using default_policy = serial_host_policy;
-
-namespace policies {
-
-inline constexpr serial_host_policy serial_host;
-inline constexpr parallel_host_policy parallel_host;
-inline constexpr parallel_device_policy parallel_device;
-
-template <typename ExecSpace>
-constexpr auto policy([[maybe_unused]] ExecSpace exec_space)
-{
-    if constexpr (std::is_same_v<ExecSpace, Kokkos::Serial>) {
-        return ddc::policies::serial_host;
-#ifdef KOKKOS_ENABLE_OPENMP
-    } else if constexpr (std::is_same_v<ExecSpace, Kokkos::OpenMP>) {
-        return ddc::policies::parallel_host;
-#endif
-#if defined(KOKKOS_ENABLE_CUDA) || defined(KOKKOS_ENABLE_HIP)
-    } else {
-        return ddc::policies::parallel_device;
-#endif
-    }
-}
-
-} // namespace policies
-
-/** iterates over a nD domain using the default execution policy
- * @param[in] domain the domain over which to iterate
- * @param[in] f      a functor taking an index as parameter
- */
-template <class... DDims, class Functor>
-inline void for_each(DiscreteDomain<DDims...> const& domain, Functor&& f) noexcept
-{
-    for_each(default_policy(), domain, std::forward<Functor>(f));
-}
-
-/** iterates over a nD extent using the default execution policy
- * @param[in] extent the extent over which to iterate
- * @param[in] f      a functor taking an index as parameter
- */
-template <class... DDims, class Functor>
-inline void for_each_n(DiscreteVector<DDims...> const& extent, Functor&& f) noexcept
-{
-    for_each_n(default_policy(), extent, std::forward<Functor>(f));
+    detail::for_each_kokkos<ExecSpace>(domain, std::forward<Functor>(f));
 }
 
 template <
-        class ExecutionPolicy,
+        class ExecSpace,
         class ElementType,
         class... DDims,
         class LayoutPolicy,
         class Functor>
 inline void for_each_elem(
-        ExecutionPolicy&& policy,
+        ExecSpace&& kokkos_execution_space,
         ChunkSpan<ElementType, DiscreteDomain<DDims...>, LayoutPolicy> chunk_span,
         Functor&& f) noexcept
 {
-    for_each(std::forward<ExecutionPolicy>(policy), chunk_span.domain(), std::forward<Functor>(f));
+    for_each(std::forward<ExecSpace>(kokkos_execution_space), chunk_span.domain(), std::forward<Functor>(f));
 }
 
 template <class ElementType, class... DDims, class LayoutPolicy, class Functor>

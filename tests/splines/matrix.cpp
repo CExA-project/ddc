decd9dd12619516718b1bca18181dc31d4f9a254
blegouix <stilynx51@gmail.com>
2024-01-31 19:15:53 +0100

    wip

--- tests/splines/matrix.cpp
+++ tests/splines/matrix.cpp
@@ -25,7 +25,6 @@ void fill_identity(ddc::DSpan2D mat)
     }
 }
 
-/*
 void copy_matrix(ddc::DSpan2D copy, std::unique_ptr<ddc::detail::Matrix>& mat)
 {
     assert(mat->get_size() == int(copy.extent(0)));
@@ -37,7 +36,6 @@ void copy_matrix(ddc::DSpan2D copy, std::unique_ptr<ddc::detail::Matrix>& mat)
         }
     }
 }
-*/
 
 void check_inverse(ddc::DSpan2D matrix, ddc::DSpan2D inv)
 {
@@ -59,6 +57,229 @@ void check_inverse(ddc::DSpan2D matrix, ddc::DSpan2D inv)
 class MatrixSizesFixture : public testing::TestWithParam<std::tuple<std::size_t, std::size_t>>
 {
 };
+
+TEST_P(MatrixSizesFixture, PositiveDefiniteSymmetric)
+{
+    auto const [N, k] = GetParam();
+    std::unique_ptr<ddc::detail::Matrix> matrix
+            = ddc::detail::MatrixMaker::make_new_banded(N, k, k, true);
+
+    for (std::size_t i(0); i < N; ++i) {
+        matrix->set_element(i, i, 2.0 * k);
+        for (std::size_t j(std::max(0, int(i) - int(k))); j < i; ++j) {
+            matrix->set_element(i, j, -1.0);
+        }
+        for (std::size_t j(i + 1); j < std::min(N, i + k + 1); ++j) {
+            matrix->set_element(i, j, -1.0);
+        }
+    }
+    std::vector<double> val_ptr(N * N);
+    ddc::DSpan2D val(val_ptr.data(), N, N);
+    copy_matrix(val, matrix);
+
+    std::vector<double> inv_ptr(N * N);
+    ddc::DSpan2D inv(inv_ptr.data(), N, N);
+    fill_identity(inv);
+    matrix->factorize();
+    matrix->solve_multiple_inplace(inv);
+    check_inverse(val, inv);
+}
+
+TEST_P(MatrixSizesFixture, OffsetBanded)
+{
+    auto const [N, k] = GetParam();
+    std::unique_ptr<ddc::detail::Matrix> matrix
+            = ddc::detail::MatrixMaker::make_new_banded(N, 0, 2 * k, true);
+
+    for (std::size_t i(0); i < N; ++i) {
+        for (std::size_t j(i); j < std::min(N, i + k); ++j) {
+            matrix->set_element(i, i, -1.0);
+        }
+        if (i + k < N) {
+            matrix->set_element(i, i + k, 2.0 * k);
+        }
+        for (std::size_t j(i + k + 1); j < std::min(N, i + k + 1); ++j) {
+            matrix->set_element(i, j, -1.0);
+        }
+    }
+    std::vector<double> val_ptr(N * N);
+    ddc::DSpan2D val(val_ptr.data(), N, N);
+    copy_matrix(val, matrix);
+
+    std::vector<double> inv_ptr(N * N);
+    ddc::DSpan2D inv(inv_ptr.data(), N, N);
+    fill_identity(inv);
+    matrix->factorize();
+    matrix->solve_multiple_inplace(inv);
+    check_inverse(val, inv);
+}
+
+TEST_P(MatrixSizesFixture, PeriodicBanded)
+{
+    auto const [N, k] = GetParam();
+
+    for (int s(-k); s < k + 1; ++s) {
+        if (s == 0)
+            continue;
+
+        std::unique_ptr<ddc::detail::Matrix> matrix
+                = ddc::detail::MatrixMaker::make_new_periodic_banded(N, k - s, k + s, false);
+        for (int i(0); i < N; ++i) {
+            for (int j(0); j < N; ++j) {
+                int diag = ddc::detail::modulo(j - i, int(N));
+                if (diag == s || diag == N + s) {
+                    matrix->set_element(i, j, 0.5);
+                } else if (diag <= s + k || diag >= N + s - k) {
+                    matrix->set_element(i, j, -1.0 / k);
+                }
+            }
+        }
+        std::vector<double> val_ptr(N * N);
+        ddc::DSpan2D val(val_ptr.data(), N, N);
+        copy_matrix(val, matrix);
+
+        std::vector<double> inv_ptr(N * N);
+        ddc::DSpan2D inv(inv_ptr.data(), N, N);
+        fill_identity(inv);
+        matrix->factorize();
+        matrix->solve_multiple_inplace(inv);
+        check_inverse(val, inv);
+    }
+}
+
+TEST_P(MatrixSizesFixture, Sparse)
+{
+    auto const [N, k] = GetParam();
+
+
+    std::unique_ptr<ddc::detail::Matrix> matrix
+            = ddc::detail::MatrixMaker::make_new_sparse<Kokkos::DefaultExecutionSpace>(N);
+
+    std::vector<double> val_ptr(N * N);
+    ddc::DSpan2D val(val_ptr.data(), N, N);
+    for (int i(0); i < N; ++i) {
+        for (int j(0); j < N; ++j) {
+            if (i == j) {
+                matrix->set_element(i, j, 2. / 3);
+                val(i, j) = 2. / 3;
+            } else if (std::abs(j - i) <= k) {
+                matrix->set_element(i, j, (1. / 3) / k);
+                val(i, j) = (1. / 3) / k;
+            } else {
+                val(i, j) = 0.;
+            }
+        }
+    }
+    // copy_matrix(val, matrix); // copy_matrix is not available for sparse matrix because of a limitation of Ginkgo API (get_element is not implemented). The workaround is to fill val directly in the loop
+
+    Kokkos::DualView<double*> inv_ptr("inv_ptr", N * N);
+    ddc::DSpan2D inv(inv_ptr.h_view.data(), N, N);
+    fill_identity(inv);
+    inv_ptr.modify_host();
+    inv_ptr.sync_device();
+    matrix->factorize();
+    matrix->solve_multiple_inplace(ddc::DSpan2D(inv_ptr.d_view.data(), N, N));
+    inv_ptr.modify_device();
+    inv_ptr.sync_host();
+    check_inverse(val, inv);
+}
+TEST_P(MatrixSizesFixture, PositiveDefiniteSymmetricTranspose)
+{
+    auto const [N, k] = GetParam();
+    std::unique_ptr<ddc::detail::Matrix> matrix
+            = ddc::detail::MatrixMaker::make_new_banded(N, k, k, true);
+
+    for (std::size_t i(0); i < N; ++i) {
+        matrix->set_element(i, i, 2.0 * k);
+        for (std::size_t j(std::max(0, int(i) - int(k))); j < i; ++j) {
+            matrix->set_element(i, j, -1.0);
+        }
+        for (std::size_t j(i + 1); j < std::min(N, i + k + 1); ++j) {
+            matrix->set_element(i, j, -1.0);
+        }
+    }
+    std::vector<double> val_ptr(N * N);
+    ddc::DSpan2D val(val_ptr.data(), N, N);
+    copy_matrix(val, matrix);
+
+    std::vector<double> inv_ptr(N * N);
+    ddc::DSpan2D inv(inv_ptr.data(), N, N);
+    fill_identity(inv);
+    matrix->factorize();
+    for (std::size_t i(0); i < N; ++i) {
+        ddc::DSpan1D inv_line(inv_ptr.data() + i * N, N);
+        matrix->solve_transpose_inplace(inv_line);
+    }
+    check_inverse_transpose(val, inv);
+}
+
+TEST_P(MatrixSizesFixture, OffsetBandedTranspose)
+{
+    auto const [N, k] = GetParam();
+    std::unique_ptr<ddc::detail::Matrix> matrix
+            = ddc::detail::MatrixMaker::make_new_banded(N, 0, 2 * k, true);
+
+    for (std::size_t i(0); i < N; ++i) {
+        for (std::size_t j(i); j < std::min(N, i + k); ++j) {
+            matrix->set_element(i, i, -1.0);
+        }
+        if (i + k < N) {
+            matrix->set_element(i, i + k, 2.0 * k);
+        }
+        for (std::size_t j(i + k + 1); j < std::min(N, i + k + 1); ++j) {
+            matrix->set_element(i, j, -1.0);
+        }
+    }
+    std::vector<double> val_ptr(N * N);
+    ddc::DSpan2D val(val_ptr.data(), N, N);
+    copy_matrix(val, matrix);
+
+    std::vector<double> inv_ptr(N * N);
+    ddc::DSpan2D inv(inv_ptr.data(), N, N);
+    fill_identity(inv);
+    matrix->factorize();
+    for (std::size_t i(0); i < N; ++i) {
+        ddc::DSpan1D inv_line(inv_ptr.data() + i * N, N);
+        matrix->solve_transpose_inplace(inv_line);
+    }
+    check_inverse_transpose(val, inv);
+}
+
+TEST_P(MatrixSizesFixture, PeriodicBandedTranspose)
+{
+    auto const [N, k] = GetParam();
+
+    for (int s(-k); s < k + 1; ++s) {
+        if (s == 0)
+            continue;
+
+        std::unique_ptr<ddc::detail::Matrix> matrix
+                = ddc::detail::MatrixMaker::make_new_periodic_banded(N, k - s, k + s, false);
+        for (int i(0); i < N; ++i) {
+            for (int j(0); j < N; ++j) {
+                int diag = ddc::detail::modulo(j - i, int(N));
+                if (diag == s || diag == N + s) {
+                    matrix->set_element(i, j, 0.5);
+                } else if (diag <= s + k || diag >= N + s - k) {
+                    matrix->set_element(i, j, -1.0 / k);
+                }
+            }
+        }
+        std::vector<double> val_ptr(N * N);
+        ddc::DSpan2D val(val_ptr.data(), N, N);
+        copy_matrix(val, matrix);
+
+        std::vector<double> inv_ptr(N * N);
+        ddc::DSpan2D inv(inv_ptr.data(), N, N);
+        fill_identity(inv);
+        matrix->factorize();
+        for (int i(0); i < N; ++i) {
+            ddc::DSpan1D inv_line(inv_ptr.data() + i * N, N);
+            matrix->solve_transpose_inplace(inv_line);
+        }
+        check_inverse_transpose(val, inv);
+    }
+}
 
 TEST_P(MatrixSizesFixture, Sparse)
 {

381583b9adbcc0bc04eb8ef2a679a44c09fda4b5
blegouix <stilynx51@gmail.com>
2024-02-08 09:36:24 +0100

    pds_tridiagonal

--- include/ddc/kernels/splines/matrix_maker.hpp
+++ include/ddc/kernels/splines/matrix_maker.hpp
@@ -30,7 +30,7 @@ public:
             bool const pds)
     {
         if (kl == ku && kl == 1 && pds) {
-            return std::make_unique<Matrix_PDS_Tridiag>(n);
+            return std::make_unique<Matrix_PDS_Tridiag<ExecSpace>>(n);
         } else if (2 * kl + 1 + ku >= n) {
             return std::make_unique<Matrix_Dense<ExecSpace>>(n);
         } else {
@@ -49,8 +49,7 @@ public:
         int const banded_size = n - border_size;
         std::unique_ptr<Matrix> block_mat;
         if (pds && kl == ku && kl == 1) {
-            // block_mat = std::make_unique<Matrix_PDS_Tridiag>(banded_size);
-            block_mat = std::make_unique<Matrix_Banded<ExecSpace>>(banded_size, 1, 1);
+            block_mat = std::make_unique<Matrix_PDS_Tridiag<ExecSpace>>(banded_size);
         } else if (
                 border_size * n + border_size * (border_size + 1) + (2 * kl + 1 + ku) * banded_size
                 >= n * n) {
@@ -73,8 +72,7 @@ public:
         int const banded_size = n - block1_size - block2_size;
         std::unique_ptr<Matrix> block_mat;
         if (pds && kl == ku && kl == 1) {
-            //block_mat = std::make_unique<Matrix_PDS_Tridiag>(banded_size);
-            block_mat = std::make_unique<Matrix_Banded<ExecSpace>>(banded_size, 1, 1);
+            block_mat = std::make_unique<Matrix_PDS_Tridiag<ExecSpace>>(banded_size);
         } else if (2 * kl + 1 + ku >= banded_size) {
             return std::make_unique<Matrix_Dense<ExecSpace>>(n);
         } else {
--- include/ddc/kernels/splines/matrix_pds_tridiag.hpp
+++ include/ddc/kernels/splines/matrix_pds_tridiag.hpp
@@ -20,72 +20,148 @@ extern "C" int dpttrs_(
         int const* ldb,
         int* info);
 
+template <class ExecSpace>
 class Matrix_PDS_Tridiag : public Matrix
 {
     /*
      * Represents a real symmetric positive definite matrix
      * stored in a block format
      * */
+protected:
+    Kokkos::View<double*, typename ExecSpace::memory_space> m_d; // diagonal
+    Kokkos::View<double*, typename ExecSpace::memory_space> m_l; // lower diagonal
+
 public:
-    Matrix_PDS_Tridiag(int const n)
-        : Matrix(n)
-        , d(std::make_unique<double[]>(n))
-        , l(std::make_unique<double[]>(n - 1))
+    Matrix_PDS_Tridiag(int const mat_size)
+        : Matrix(mat_size)
+        , m_d("d", mat_size)
+        , m_l("l", mat_size - 1)
     {
-    }
-    void reset() const override
-    {
-        memset(d.get(), 0, get_size() * sizeof(double));
-        memset(l.get(), 0, (get_size() - 1) * sizeof(double));
     }
 
-    double KOKKOS_FUNCTION get_element(int i, int j) const override
+    void reset() const override
+    {
+        Kokkos::parallel_for(
+                "fill_d_l",
+                Kokkos::RangePolicy<ExecSpace>(0, get_size()),
+                KOKKOS_CLASS_LAMBDA(const int i) {
+                    m_d(i) = 0;
+                    if (i < get_size() - 1) {
+                        m_l(i) = 0;
+                    }
+                });
+    }
+
+    KOKKOS_FUNCTION double get_element(int i, int j) const override
     {
         if (i == j) {
-            return d[i];
+            KOKKOS_IF_ON_HOST(
+                    if constexpr (Kokkos::SpaceAccessibility<
+                                          Kokkos::DefaultHostExecutionSpace,
+                                          typename ExecSpace::memory_space>::accessible) {
+                        return m_d(i);
+                    } else {
+                        // Inefficient, usage is strongly discouraged
+                        double aij;
+                        Kokkos::deep_copy(
+                                Kokkos::View<double, Kokkos::HostSpace>(&aij),
+                                Kokkos::subview(m_d, i));
+                        return aij;
+                    })
+            KOKKOS_IF_ON_DEVICE(return m_d(i);)
         }
         if (i > j) {
-            std::swap(i, j);
+            // inline swap i<->j
+            int tmp = i;
+            i = j;
+            j = tmp;
         }
         if (i + 1 == j) {
-            return l[i];
+            KOKKOS_IF_ON_HOST(
+                    if constexpr (Kokkos::SpaceAccessibility<
+                                          Kokkos::DefaultHostExecutionSpace,
+                                          typename ExecSpace::memory_space>::accessible) {
+                        return m_l(i);
+                    } else {
+                        // Inefficient, usage is strongly discouraged
+                        double aij;
+                        Kokkos::deep_copy(
+                                Kokkos::View<double, Kokkos::HostSpace>(&aij),
+                                Kokkos::subview(m_l, i));
+                        return aij;
+                    })
+            KOKKOS_IF_ON_DEVICE(return m_l(i);)
         }
         return 0.0;
     }
-    void KOKKOS_FUNCTION set_element(int i, int j, double const a_ij) const override
+    KOKKOS_FUNCTION void set_element(int i, int j, double const aij) const override
     {
         if (i == j) {
-            d[i] = a_ij;
+            KOKKOS_IF_ON_HOST(
+                    if constexpr (Kokkos::SpaceAccessibility<
+                                          Kokkos::DefaultHostExecutionSpace,
+                                          typename ExecSpace::memory_space>::accessible) {
+                        m_d(i) = aij;
+                    } else {
+                        // Inefficient, usage is strongly discouraged
+                        Kokkos::deep_copy(
+                                Kokkos::subview(m_d, i),
+                                Kokkos::View<const double, Kokkos::HostSpace>(&aij));
+                    })
+            KOKKOS_IF_ON_DEVICE(m_d(i) = aij;)
             return;
         }
         if (i > j) {
-            std::swap(i, j);
+            // inline swap i<->j
+            int tmp = i;
+            i = j;
+            j = tmp;
         }
         if (i + 1 != j) {
-            assert(std::fabs(a_ij) < 1e-20);
+            assert(std::fabs(aij) < 1e-20);
         } else {
-            l[i] = a_ij;
+            KOKKOS_IF_ON_HOST(
+                    if constexpr (Kokkos::SpaceAccessibility<
+                                          Kokkos::DefaultHostExecutionSpace,
+                                          typename ExecSpace::memory_space>::accessible) {
+                        m_l(i) = aij;
+                    } else {
+                        // Inefficient, usage is strongly discouraged
+                        Kokkos::deep_copy(
+                                Kokkos::subview(m_l, i),
+                                Kokkos::View<const double, Kokkos::HostSpace>(&aij));
+                    })
+            KOKKOS_IF_ON_DEVICE(m_l(i) = aij;)
         }
     }
 
 protected:
     int factorize_method() override
     {
+        auto d_host = create_mirror_view_and_copy(Kokkos::DefaultHostExecutionSpace(), m_d);
+        auto l_host = create_mirror_view_and_copy(Kokkos::DefaultHostExecutionSpace(), m_l);
         int info;
         int const n = get_size();
-        dpttrf_(&n, d.get(), l.get(), &info);
+        dpttrf_(&n, d_host.data(), l_host.data(), &info);
+        Kokkos::deep_copy(m_d, d_host);
+        Kokkos::deep_copy(m_l, l_host);
         return info;
     }
+
     int solve_inplace_method(double* const b, char const, int const n_equations, int const stride)
             const override
     {
+        auto d_host = create_mirror_view_and_copy(Kokkos::DefaultHostExecutionSpace(), m_d);
+        auto l_host = create_mirror_view_and_copy(Kokkos::DefaultHostExecutionSpace(), m_l);
+        Kokkos::View<double**, Kokkos::LayoutLeft, typename ExecSpace::memory_space>
+                b_view(b, get_size(), n_equations);
+        auto b_host = create_mirror_view_and_copy(Kokkos::DefaultHostExecutionSpace(), b_view);
         int info;
         int const n = get_size();
-        dpttrs_(&n, &n_equations, d.get(), l.get(), b, &n, &info);
+        dpttrs_(&n, &n_equations, d_host.data(), l_host.data(), b_host.data(), &n, &info);
+        Kokkos::deep_copy(b_view, b_host);
         return info;
     }
-    std::unique_ptr<double[]> d; // diagonal
-    std::unique_ptr<double[]> l; // lower diagonal
 };
 
 } // namespace ddc::detail

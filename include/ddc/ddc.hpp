042e6dab64b1812861adc62ea43de37b14a0ef5e
Thomas Padioleau <thomas.padioleau@cea.fr>
2024-03-05 19:13:43 +0100

    Rename deepcopy files to parallel_deepcopy

--- include/ddc/ddc.hpp
+++ include/ddc/ddc.hpp
@@ -25,9 +25,9 @@
 #include "ddc/uniform_point_sampling.hpp"
 
 // Algorithms
-#include "ddc/deepcopy.hpp"
 #include "ddc/for_each.hpp"
 #include "ddc/mirror.hpp"
+#include "ddc/parallel_deepcopy.hpp"
 #include "ddc/parallel_fill.hpp"
 #include "ddc/parallel_for_each.hpp"
 #include "ddc/reducer.hpp"

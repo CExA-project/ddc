7c6379916890adffb93c998d6845d044dff560b9
Thomas Padioleau <thomas.padioleau@cea.fr>
2024-03-06 09:29:31 +0100

    Review from Yuuichi

--- include/ddc/discrete_space.hpp
+++ include/ddc/discrete_space.hpp
@@ -197,8 +197,8 @@ std::enable_if_t<2 <= sizeof...(Args), std::tuple<Args...>> init_discrete_space(
 /**
  * @tparam DDim a discrete dimension
  * @return the discrete space instance associated with `DDim`.
- * It is recommended to call this function from a `KOKKOS_FUNCTION`.
- * Prefer `ddc::host_discrete_space` for a host-only function.
+ * This function must be called from a `KOKKOS_FUNCTION`.
+ * Call `ddc::host_discrete_space` for a host-only function instead.
  */
 template <class DDim, class MemorySpace = DDC_CURRENT_KOKKOS_SPACE>
 KOKKOS_FORCEINLINE_FUNCTION detail::ddim_impl_t<DDim, MemorySpace> const& discrete_space()

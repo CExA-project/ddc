2a519efdae17c344c76f3e43c2b362520e2502c8
tpadioleau <tpadioleau@noreply.example.com>
2024-03-15 14:34:02 +0000

    Update to match 638d138e69756a9954e692464f3b7cf19df15dcf by push 1687

--- classddc_1_1DualDiscretization.js
+++ classddc_1_1DualDiscretization.js
@@ -1,7 +1,7 @@
 var classddc_1_1DualDiscretization =
 [
     [ "DualDiscretization", "classddc_1_1DualDiscretization.html#a02e81424c69b18a9a26e733d03ad3575", null ],
-    [ "get", "classddc_1_1DualDiscretization.html#a2f613b516407334ecaf55e7eb4de9d37", null ],
-    [ "get_host", "classddc_1_1DualDiscretization.html#adc5cec7f32722515f62dac7f9cd74df1", null ],
-    [ "get_device", "classddc_1_1DualDiscretization.html#a49f6cb6ad60c3ce0032cff0eda5dd7ba", null ]
+    [ "get", "classddc_1_1DualDiscretization.html#a918909c5b372ba3944c18a478023703f", null ],
+    [ "get_host", "classddc_1_1DualDiscretization.html#a70ab185179fc1e514cbfc2b6015ee74c", null ],
+    [ "get_device", "classddc_1_1DualDiscretization.html#adae22e0ad03b7ec83ba3e29063b42402", null ]
 ];

83b444e7f40db082350661f3ed7b938915bb2996
thierryantoun <152993060+thierryantoun@users.noreply.github.com>
2024-04-17 10:35:39 +0200

    Update examples/uniform_heat_equation.cpp
    Co-authored-by: Thomas Padioleau <thomas.padioleau@cea.fr>

--- examples/uniform_heat_equation.cpp
+++ examples/uniform_heat_equation.cpp
@@ -258,10 +258,8 @@ int main(int argc, char** argv)
                 next_temp.domain(),
                 KOKKOS_LAMBDA(
                         ddc::DiscreteElement<DDimX, DDimY> const ixy) {
-                    ddc::DiscreteElement<DDimX> const ix
-                            = ddc::select<DDimX>(ixy);
-                    ddc::DiscreteElement<DDimY> const iy
-                            = ddc::select<DDimY>(ixy);
+                    ddc::DiscreteElement<DDimX> const ix(ixy);
+                    ddc::DiscreteElement<DDimY> const iy(ixy);
                     double const dt = ddc::step<DDimT>();
 
                     next_temp(ix, iy) = last_temp(ix, iy);

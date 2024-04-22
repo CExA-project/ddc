49b9b01a01751445c260bb219c9c84f62031c1fc
blegouix <stilynx51@gmail.com>
2024-04-22 12:38:04 +0200

    minor

--- include/ddc/kernels/splines/bsplines_non_uniform.hpp
+++ include/ddc/kernels/splines/bsplines_non_uniform.hpp
@@ -92,13 +92,13 @@ public:
         /// @brief The type of the discrete dimension representing the B-splines.
         using discrete_dimension_type = NonUniformBSplines;
 
-        /// @brief The type of a discrete domain whose elements identify the B-splines.
+        /// @brief The type of a DiscreteDomain whose elements identify the B-splines.
         using discrete_domain_type = DiscreteDomain<DDim>;
 
-        /// @brief The type of a discrete element identifying a B-spline.
+        /// @brief The type of a DiscreteElement identifying a B-spline.
         using discrete_element_type = DiscreteElement<DDim>;
 
-        /// @brief The type of a discrete vector representing an "index displacement" between two B-splines.
+        /// @brief The type of a DiscreteVector representing an "index displacement" between two B-splines.
         using discrete_vector_type = DiscreteVector<DDim>;
 
         Impl() = default;

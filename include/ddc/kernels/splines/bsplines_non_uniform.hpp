29a9c3c98ef49f8915d0e95aa7748babfc85bd63
blegouix <stilynx51@gmail.com>
2024-04-23 09:50:36 +0200

    wip

--- include/ddc/kernels/splines/bsplines_non_uniform.hpp
+++ include/ddc/kernels/splines/bsplines_non_uniform.hpp
@@ -114,6 +114,8 @@ public:
          * The brace-list is the list of break points. It is used to build the DiscreteDomain indexing
          * the knots and iterated over to build the knots coordinates list and initialize the associated DiscreteSpace.
          *
+         * @see Impl(RandomIt breaks_begin, RandomIt breaks_end)
+         *
          * @param breaks The std::initializer_list of the coordinates of break points.
          */
         explicit Impl(std::initializer_list<ddc::Coordinate<Tag>> breaks)
@@ -125,6 +127,8 @@ public:
          *
          * The std::vector is the list of break points. It is used to build the DiscreteDomain indexing
          * the knots and iterated over to build the knots coordinates list and initialize the associated DiscreteSpace.
+         *
+         * @see Impl(RandomIt breaks_begin, RandomIt breaks_end)
          *
          * @param breaks The std::vector of the coordinates of break points.
          */
@@ -204,7 +208,7 @@ public:
          * The derivatives are computed for every B-splines with support at the given coordinate x. There are only (degree+1)
          * B-splines which are non-zero at any given point. It is these B-splines which are derivated.
          * A spline approximation of a derivative at coordinate x is a linear
-         * combination of those B-splines derivatives weigthed with splines coefficients of the spline-transformed
+         * combination of those B-splines derivatives weighted with splines coefficients of the spline-transformed
          * initial discrete function.
          *
          * @param[out] derivs The derivatives of the B-splines evaluated at coordinate x. It has to be a 1D mdspan with (degree+1) elements.
@@ -216,10 +220,10 @@ public:
 
         /** @brief Evaluates non-zero B-splines values and \f$n\f$ derivatives at a given coordinate
          *
-         * The values and derivatives are computed for every B-splines with support at the given coordinate x. There are only (degree+1)
-         * B-splines which are non-zero at any given point. It is these B-splines which are derivated.
+         * The values and derivatives are computed for every B-spline with support at the given coordinate x. There are only (degree+1)
+         * B-splines which are non-zero at any given point. It is these B-splines which are evaluated and derivated.
          * A spline approximation of a derivative at coordinate x is a linear
-         * combination of those B-splines derivatives weigthed with splines coefficients of the spline-transformed
+         * combination of those B-splines derivatives weighted with splines coefficients of the spline-transformed
          * initial discrete function.
          *
          * @param[out] derivs The values and \f$n\f$ derivatives of the B-splines evaluated at coordinate x. It has to be a 2D mdspan with (degree+1)*(n+1) elements.

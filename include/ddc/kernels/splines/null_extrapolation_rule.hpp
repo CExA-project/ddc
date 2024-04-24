0e27bfeaff554e34c7a588ab751e05f370d57791
blegouix <stilynx51@gmail.com>
2024-04-24 13:27:48 +0200

    null_extrap

--- include/ddc/kernels/splines/null_extrapolation_rule.hpp
+++ include/ddc/kernels/splines/null_extrapolation_rule.hpp
@@ -11,6 +11,16 @@ namespace ddc {
  */
 struct NullExtrapolationRule
 {
+    /**
+     * @brief Evaluates 0. out of the domain.
+     *
+     * @param[in] pos
+     *          The coordinate where we want to evaluate the function on B-splines.
+     * @param[in] spline_coef
+     *          The coefficients of the function on B-splines.
+     *
+     * @return A double with the value of the function on B-splines evaluated at the coordinate (here, 0.).
+     */
     template <class CoordType, class ChunkSpan>
     KOKKOS_FUNCTION double operator()(CoordType, ChunkSpan) const
     {

a972961c03d541446fef146f37d4badd34394797
blegouix <stilynx51@gmail.com>
2024-04-25 14:41:28 +0200

    remove null_extrapolation

--- include/ddc/kernels/splines/null_extrapolation_rule.hpp
+++ include/ddc/kernels/splines/null_extrapolation_rule.hpp
@@ -6,16 +6,8 @@
 
 namespace ddc {
 
-/**
- * @brief A functor for describing a spline boundary value by a null extrapolation for 1D evaluator.
- */
 struct NullExtrapolationRule
 {
-    /**
-     * @brief Evaluates 0. out of the domain.
-     *
-     * @return A double with the value of the function on B-splines evaluated at the coordinate (here, 0.).
-     */
     template <class CoordType, class ChunkSpan>
     KOKKOS_FUNCTION double operator()(CoordType, ChunkSpan) const
     {

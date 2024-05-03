095635e1a8e5fe364ad00f71752fadd3419171c8
blegouix <stilynx51@gmail.com>
2024-05-03 17:18:09 +0200

    Emily's review

--- include/ddc/kernels/splines/spline_builder.hpp
+++ include/ddc/kernels/splines/spline_builder.hpp
@@ -108,7 +108,8 @@ public:
 	 * @brief The type of the batch domain (obtained by removing the dimension of interest
 	 * from the whole domain).
 	 *
-	 * Example: For batched_interpolation_domain_type = DiscreteDomain<X,Y,Z> and a dimension of interest Y, this is DiscreteDomain<X,Z>
+	 * Example: For batched_interpolation_domain_type = DiscreteDomain<X,Y,Z> and a dimension of interest Y,
+     * this is DiscreteDomain<X,Z>
 	 */
     using batch_domain_type =
             typename ddc::detail::convert_type_seq_to_discrete_domain<ddc::type_seq_remove_t<
@@ -146,7 +147,8 @@ public:
      * @brief The type of the whole Deriv domain (cartesian product of 1D Deriv domain 
      * and batch domain) preserving the underlying memory layout (order of dimensions).
      *
-     * Example: For batched_interpolation_domain_type = DiscreteDomain<X,Y,Z> and a dimension of interest Y, this is DiscreteDomain<X,Deriv<Y>,Z>
+     * Example: For batched_interpolation_domain_type = DiscreteDomain<X,Y,Z> and a dimension of interest Y,
+     * this is DiscreteDomain<X,Deriv<Y>,Z>
      */
     using batched_derivs_domain_type =
             typename ddc::detail::convert_type_seq_to_discrete_domain<ddc::type_seq_replace_t<
@@ -275,7 +277,7 @@ public:
 
     /**
      * @brief Get the batch domain.
-	 *
+     *
      * Obtained by removing the dimension of interest from the whole interpolation domain.
      *
      * @return The batch domain.
@@ -314,7 +316,7 @@ public:
     /**
      * @brief Get the whole domain on which spline coefficients are defined, with the dimension of interest being the leading dimension.
      *
-     * This is used internally because of solvers limitation and because it may be beneficial to computation performance.
+     * This is used internally due to solver limitation and because it may be beneficial to computation performance.
      *
      * @return The (transposed) domain for the spline coefficients.
      */

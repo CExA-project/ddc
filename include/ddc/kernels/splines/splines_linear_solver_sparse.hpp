4dd4564fba85284c2a3198512ae3e6137a167728
Thomas Padioleau <thomas.padioleau@cea.fr>
2024-05-17 16:19:53 +0200

    Update include/ddc/kernels/splines/splines_linear_solver_sparse.hpp

--- include/ddc/kernels/splines/splines_linear_solver_sparse.hpp
+++ include/ddc/kernels/splines/splines_linear_solver_sparse.hpp
@@ -178,8 +178,7 @@ public:
         m_matrix_sparse = matrix_sparse_type::create(gko_exec, gko::dim<2>(mat_size, mat_size));
     }
 
-    virtual double get_element(std::size_t i, std::size_t j)
-            const override
+    virtual double get_element(std::size_t i, std::size_t j) const override
     {
         return m_matrix_dense->at(i, j);
     }

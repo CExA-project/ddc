25bb2247566452a9c9a067983d8e4677524f5204
blegouix <stilynx51@gmail.com>
2024-05-22 10:53:58 +0200

    maybe unused

--- include/ddc/kernels/splines/splines_linear_problem_maker.hpp
+++ include/ddc/kernels/splines/splines_linear_problem_maker.hpp
@@ -45,10 +45,10 @@ public:
      */
     template <typename ExecSpace>
     static std::unique_ptr<SplinesLinearProblem<ExecSpace>> make_new_band(
-            int const n,
-            int const kl,
-            int const ku,
-            bool const pds)
+            [[maybe_unused]] int const n,
+            [[maybe_unused]] int const kl,
+            [[maybe_unused]] int const ku,
+            [[maybe_unused]] bool const pds)
     {
         // Placeholder while SplinesLinearProblemBand is not implemented
         return std::make_unique<SplinesLinearProblemDense<ExecSpace>>(n);

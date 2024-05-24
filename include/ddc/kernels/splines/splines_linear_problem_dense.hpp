fe9c1cea61780a36b2a2395b05bb7c7292db8bbf
blegouix <stilynx51@gmail.com>
2024-05-24 16:04:54 +0200

    clang-format

--- include/ddc/kernels/splines/splines_linear_problem_dense.hpp
+++ include/ddc/kernels/splines/splines_linear_problem_dense.hpp
@@ -8,11 +8,11 @@
 #include <memory>
 #include <string>
 
-# if __has_include (<mkl_lapacke.h>)
-#   include <mkl_lapacke.h>
-# else
-#   include <lapacke.h>
-# endif
+#if __has_include(<mkl_lapacke.h>)
+#include <mkl_lapacke.h>
+#else
+#include <lapacke.h>
+#endif
 
 #include "splines_linear_problem.hpp"
 

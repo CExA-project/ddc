fdbfecf284f452f6d38f6569df1b7ca0422e77af
blegouix <stilynx51@gmail.com>
2024-05-30 10:03:41 +0200

    renaming

--- include/ddc/kernels/splines/splines_linear_problem_2x2_blocks.hpp
+++ include/ddc/kernels/splines/splines_linear_problem_2x2_blocks.hpp
@@ -22,7 +22,7 @@ namespace ddc::detail {
 /**
  * @brief A 2x2-blocks linear problem dedicated to the computation of a spline approximation, with all blocks except top-left being stored in dense format.
  *
- * The storage format is dense row-major for top-left and bottom-right blocks, the one of SplinesLinearProblemDense (which is also dense row-major in practice) for bottom-right block and undefined for the top-left one (determined by the type of q_block).
+ * The storage format is dense row-major for top-left and bottom-right blocks, the one of SplinesLinearProblemDense (which is also dense row-major in practice) for bottom-right block and undefined for the top-left one (determined by the type of top_left_block).
  *
  * This class implements a Schur complement method to perform a block-LU factorization and solve, calling tl_block and br_block setup_solver() and solve() methods for internal operations.
  *
@@ -38,14 +38,14 @@ public:
 protected:
     //-------------------------------------
     //
-    //    q = | q_block | gamma |
-    //        |  lambda | delta |
+    //    q = | top_left_block | top_right_block |
+    //        |  bottom_left_block | bottom_right_block |
     //
     //-------------------------------------
-    std::shared_ptr<SplinesLinearProblem<ExecSpace>> m_q_block;
-    std::shared_ptr<SplinesLinearProblem<ExecSpace>> m_delta;
-    Kokkos::View<double**, Kokkos::HostSpace> m_Abm_1_gamma;
-    Kokkos::View<double**, Kokkos::HostSpace> m_lambda;
+    std::shared_ptr<SplinesLinearProblem<ExecSpace>> m_top_left_block;
+    std::shared_ptr<SplinesLinearProblem<ExecSpace>> m_bottom_right_block;
+    Kokkos::View<double**, Kokkos::HostSpace> m_top_right_block;
+    Kokkos::View<double**, Kokkos::HostSpace> m_bottom_left_block;
 
 public:
     /**
@@ -58,15 +58,21 @@ public:
             std::size_t const k,
             std::unique_ptr<SplinesLinearProblem<ExecSpace>> q)
         : SplinesLinearProblem<ExecSpace>(mat_size)
-        , m_q_block(std::move(q))
-        , m_delta(new SplinesLinearProblemDense<ExecSpace>(k))
-        , m_Abm_1_gamma("Abm_1_gamma", m_q_block->size(), mat_size - m_q_block->size())
-        , m_lambda("lambda", mat_size - m_q_block->size(), m_q_block->size())
+        , m_top_left_block(std::move(q))
+        , m_bottom_right_block(new SplinesLinearProblemDense<ExecSpace>(k))
+        , m_top_right_block(
+                  "top_right_block",
+                  m_top_left_block->size(),
+                  mat_size - m_top_left_block->size())
+        , m_bottom_left_block(
+                  "bottom_left_block",
+                  mat_size - m_top_left_block->size(),
+                  m_top_left_block->size())
     {
-        assert(m_q_block->size() <= mat_size);
+        assert(m_top_left_block->size() <= mat_size);
 
-        Kokkos::deep_copy(m_Abm_1_gamma, 0.);
-        Kokkos::deep_copy(m_lambda, 0.);
+        Kokkos::deep_copy(m_top_right_block, 0.);
+        Kokkos::deep_copy(m_bottom_left_block, 0.);
     }
 
 protected:
@@ -74,18 +80,21 @@ protected:
             std::size_t const mat_size,
             std::size_t const k,
             std::unique_ptr<SplinesLinearProblem<ExecSpace>> q,
-            std::size_t const lambda_size1,
-            std::size_t const lambda_size2)
+            std::size_t const bottom_left_block_size1,
+            std::size_t const bottom_left_block_size2)
         : SplinesLinearProblem<ExecSpace>(mat_size)
-        , m_q_block(std::move(q))
-        , m_delta(new SplinesLinearProblemDense<ExecSpace>(k))
-        , m_Abm_1_gamma("Abm_1_gamma", m_q_block->size(), mat_size - m_q_block->size())
-        , m_lambda("lambda", lambda_size1, lambda_size2)
+        , m_top_left_block(std::move(q))
+        , m_bottom_right_block(new SplinesLinearProblemDense<ExecSpace>(k))
+        , m_top_right_block(
+                  "top_right_block",
+                  m_top_left_block->size(),
+                  mat_size - m_top_left_block->size())
+        , m_bottom_left_block("bottom_left_block", bottom_left_block_size1, bottom_left_block_size2)
     {
-        assert(m_q_block->size() <= mat_size);
+        assert(m_top_left_block->size() <= mat_size);
 
-        Kokkos::deep_copy(m_Abm_1_gamma, 0.);
-        Kokkos::deep_copy(m_lambda, 0.);
+        Kokkos::deep_copy(m_top_right_block, 0.);
+        Kokkos::deep_copy(m_bottom_left_block, 0.);
     }
 
 public:
@@ -94,15 +103,15 @@ public:
         assert(i < size());
         assert(j < size());
 
-        std::size_t const nq = m_q_block->size();
+        std::size_t const nq = m_top_left_block->size();
         if (i < nq && j < nq) {
-            return m_q_block->get_element(i, j);
+            return m_top_left_block->get_element(i, j);
         } else if (i >= nq && j >= nq) {
-            return m_delta->get_element(i - nq, j - nq);
+            return m_bottom_right_block->get_element(i - nq, j - nq);
         } else if (j >= nq) {
-            return m_Abm_1_gamma(i, j - nq);
+            return m_top_right_block(i, j - nq);
         } else {
-            return m_lambda(i - nq, j);
+            return m_bottom_left_block(i - nq, j);
         }
     }
 
@@ -111,32 +120,33 @@ public:
         assert(i < size());
         assert(j < size());
 
-        std::size_t const nq = m_q_block->size();
+        std::size_t const nq = m_top_left_block->size();
         if (i < nq && j < nq) {
-            m_q_block->set_element(i, j, aij);
+            m_top_left_block->set_element(i, j, aij);
         } else if (i >= nq && j >= nq) {
-            m_delta->set_element(i - nq, j - nq, aij);
+            m_bottom_right_block->set_element(i - nq, j - nq, aij);
         } else if (j >= nq) {
-            m_Abm_1_gamma(i, j - nq) = aij;
+            m_top_right_block(i, j - nq) = aij;
         } else {
-            m_lambda(i - nq, j) = aij;
+            m_bottom_left_block(i - nq, j) = aij;
         }
     }
 
 private:
-    virtual void calculate_delta_to_factorize()
+    virtual void calculate_bottom_right_block_to_factorize()
     {
         Kokkos::parallel_for(
-                "calculate_delta_to_factorize",
-                Kokkos::MDRangePolicy<
-                        Kokkos::DefaultHostExecutionSpace,
-                        Kokkos::Rank<2>>({0, 0}, {m_delta->size(), m_delta->size()}),
+                "calculate_bottom_right_block_to_factorize",
+                Kokkos::MDRangePolicy<Kokkos::DefaultHostExecutionSpace, Kokkos::Rank<2>>(
+                        {0, 0},
+                        {m_bottom_right_block->size(), m_bottom_right_block->size()}),
                 [&](const int i, const int j) {
                     double val = 0.0;
-                    for (int l = 0; l < m_q_block->size(); ++l) {
-                        val += m_lambda(i, l) * m_Abm_1_gamma(l, j);
+                    for (int l = 0; l < m_top_left_block->size(); ++l) {
+                        val += m_bottom_left_block(i, l) * m_top_right_block(l, j);
                     }
-                    m_delta->set_element(i, j, m_delta->get_element(i, j) - val);
+                    m_bottom_right_block
+                            ->set_element(i, j, m_bottom_right_block->get_element(i, j) - val);
                 });
     }
 
@@ -148,22 +158,23 @@ public:
      */
     void setup_solver() override
     {
-        m_q_block->setup_solver();
-        auto Abm_1_gamma_device = create_mirror_view_and_copy(ExecSpace(), m_Abm_1_gamma);
-        m_q_block->solve(Abm_1_gamma_device);
-        deep_copy(m_Abm_1_gamma, Abm_1_gamma_device);
-        calculate_delta_to_factorize();
-        m_delta->setup_solver();
+        m_top_left_block->setup_solver();
+        auto top_right_block_device = create_mirror_view_and_copy(ExecSpace(), m_top_right_block);
+        m_top_left_block->solve(top_right_block_device);
+        deep_copy(m_top_right_block, top_right_block_device);
+        calculate_bottom_right_block_to_factorize();
+        m_bottom_right_block->setup_solver();
     }
 
     /**
      * [SHOULD BE PRIVATE (GPU programming limitation)]
      */
-    virtual void solve_lambda_section(MultiRHS const v, MultiRHS const u) const
+    virtual void solve_bottom_left_block_section(MultiRHS const v, MultiRHS const u) const
     {
-        auto lambda_device = create_mirror_view_and_copy(ExecSpace(), m_lambda);
+        auto bottom_left_block_device
+                = create_mirror_view_and_copy(ExecSpace(), m_bottom_left_block);
         Kokkos::parallel_for(
-                "solve_lambda_section",
+                "solve_bottom_left_block_section",
                 Kokkos::TeamPolicy<ExecSpace>(v.extent(1), Kokkos::AUTO),
                 KOKKOS_LAMBDA(
                         const typename Kokkos::TeamPolicy<ExecSpace>::member_type& teamMember) {
@@ -173,9 +184,9 @@ public:
                     Kokkos::parallel_for(
                             Kokkos::TeamThreadRange(teamMember, v.extent(0)),
                             [&](const int i) {
-                                // Upper diagonals in lambda
+                                // Upper diagonals in bottom_left_block
                                 for (int l = 0; l < u.extent(0); ++l) {
-                                    v(i, j) -= lambda_device(i, l) * u(l, j);
+                                    v(i, j) -= bottom_left_block_device(i, l) * u(l, j);
                                 }
                             });
                 });
@@ -184,11 +195,12 @@ public:
     /**
      * [SHOULD BE PRIVATE (GPU programming limitation)]
      */
-    virtual void solve_lambda_section_transpose(MultiRHS const u, MultiRHS const v) const
+    virtual void solve_bottom_left_block_section_transpose(MultiRHS const u, MultiRHS const v) const
     {
-        auto lambda_device = create_mirror_view_and_copy(ExecSpace(), m_lambda);
+        auto bottom_left_block_device
+                = create_mirror_view_and_copy(ExecSpace(), m_bottom_left_block);
         Kokkos::parallel_for(
-                "solve_lambda_section_transpose",
+                "solve_bottom_left_block_section_transpose",
                 Kokkos::TeamPolicy<ExecSpace>(u.extent(1), Kokkos::AUTO),
                 KOKKOS_LAMBDA(
                         const typename Kokkos::TeamPolicy<ExecSpace>::member_type& teamMember) {
@@ -198,9 +210,9 @@ public:
                     Kokkos::parallel_for(
                             Kokkos::TeamThreadRange(teamMember, u.extent(0)),
                             [&](const int i) {
-                                // Upper diagonals in lambda
+                                // Upper diagonals in bottom_left_block
                                 for (int l = 0; l < v.extent(0); ++l) {
-                                    u(i, j) -= lambda_device(l, i) * v(l, j);
+                                    u(i, j) -= bottom_left_block_device(l, i) * v(l, j);
                                 }
                             });
                 });
@@ -209,11 +221,11 @@ public:
     /**
      * [SHOULD BE PRIVATE (GPU programming limitation)]
      */
-    virtual void solve_gamma_section(MultiRHS const u, MultiRHS const v) const
+    virtual void solve_top_right_block_section(MultiRHS const u, MultiRHS const v) const
     {
-        auto Abm_1_gamma_device = create_mirror_view_and_copy(ExecSpace(), m_Abm_1_gamma);
+        auto top_right_block_device = create_mirror_view_and_copy(ExecSpace(), m_top_right_block);
         Kokkos::parallel_for(
-                "solve_gamma_section",
+                "solve_top_right_block_section",
                 Kokkos::TeamPolicy<ExecSpace>(u.extent(1), Kokkos::AUTO),
                 KOKKOS_LAMBDA(
                         const typename Kokkos::TeamPolicy<ExecSpace>::member_type& teamMember) {
@@ -223,9 +235,9 @@ public:
                     Kokkos::parallel_for(
                             Kokkos::TeamThreadRange(teamMember, u.extent(0)),
                             [&](const int i) {
-                                // Upper diagonals in lambda
+                                // Upper diagonals in bottom_left_block
                                 for (int l = 0; l < v.extent(0); ++l) {
-                                    u(i, j) -= Abm_1_gamma_device(i, l) * v(l, j);
+                                    u(i, j) -= top_right_block_device(i, l) * v(l, j);
                                 }
                             });
                 });
@@ -234,11 +246,11 @@ public:
     /**
      * [SHOULD BE PRIVATE (GPU programming limitation)]
      */
-    virtual void solve_gamma_section_transpose(MultiRHS const v, MultiRHS const u) const
+    virtual void solve_top_right_block_section_transpose(MultiRHS const v, MultiRHS const u) const
     {
-        auto Abm_1_gamma_device = create_mirror_view_and_copy(ExecSpace(), m_Abm_1_gamma);
+        auto top_right_block_device = create_mirror_view_and_copy(ExecSpace(), m_top_right_block);
         Kokkos::parallel_for(
-                "solve_gamma_section_transpose",
+                "solve_top_right_block_section_transpose",
                 Kokkos::TeamPolicy<ExecSpace>(v.extent(1), Kokkos::AUTO),
                 KOKKOS_LAMBDA(
                         const typename Kokkos::TeamPolicy<ExecSpace>::member_type& teamMember) {
@@ -248,9 +260,9 @@ public:
                     Kokkos::parallel_for(
                             Kokkos::TeamThreadRange(teamMember, v.extent(0)),
                             [&](const int i) {
-                                // Upper diagonals in lambda
+                                // Upper diagonals in bottom_left_block
                                 for (int l = 0; l < u.extent(0); ++l) {
-                                    v(i, j) -= Abm_1_gamma_device(l, i) * u(l, j);
+                                    v(i, j) -= top_right_block_device(l, i) * u(l, j);
                                 }
                             });
                 });
@@ -269,21 +281,23 @@ public:
         assert(b.extent(0) == size());
 
         MultiRHS u = Kokkos::
-                subview(b, std::pair<std::size_t, std::size_t>(0, m_q_block->size()), Kokkos::ALL);
+                subview(b,
+                        std::pair<std::size_t, std::size_t>(0, m_top_left_block->size()),
+                        Kokkos::ALL);
         MultiRHS v = Kokkos::
                 subview(b,
-                        std::pair<std::size_t, std::size_t>(m_q_block->size(), b.extent(0)),
+                        std::pair<std::size_t, std::size_t>(m_top_left_block->size(), b.extent(0)),
                         Kokkos::ALL);
         if (!transpose) {
-            m_q_block->solve(u);
-            solve_lambda_section(v, u);
-            m_delta->solve(v);
-            solve_gamma_section(u, v);
+            m_top_left_block->solve(u);
+            solve_bottom_left_block_section(v, u);
+            m_bottom_right_block->solve(v);
+            solve_top_right_block_section(u, v);
         } else {
-            solve_gamma_section_transpose(v, u);
-            m_delta->solve(v, true);
-            solve_lambda_section_transpose(u, v);
-            m_q_block->solve(u, true);
+            solve_top_right_block_section_transpose(v, u);
+            m_bottom_right_block->solve(v, true);
+            solve_bottom_left_block_section_transpose(u, v);
+            m_top_left_block->solve(u, true);
         }
     }
 };

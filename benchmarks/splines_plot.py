5dba37432a798b2f828c844ac41b3295fdf73a87
blegouix <stilynx51@gmail.com>
2024-06-18 19:54:07 +0200

    wip

--- benchmarks/splines_plot.py
+++ benchmarks/splines_plot.py
@@ -8,6 +8,7 @@
 # python3 splines_plot.py /path/to/splines_bench.json
 
 import argparse
+from operator import itemgetter 
 import matplotlib.pyplot as plt
 import json
 import numpy as np
@@ -31,21 +32,37 @@ for benchmark in data["benchmarks"]:
     data_groups["nx"][nx]["bytes_per_second"].append(benchmark["bytes_per_second"])
     data_groups["nx"][nx]["gpu_mem_occupancy"].append(benchmark["gpu_mem_occupancy"])
 
+#
+data_dict = [{
+"on_gpu": int(benchmark["name"].split("/")[1]),
+"nx": int(benchmark["name"].split("/")[4]),
+"ny": int(benchmark["name"].split("/")[5]),
+"cols_per_chunk": int(benchmark["name"].split("/")[6]),
+"preconditionner_max_block_size": int(benchmark["name"].split("/")[7]),
+"bytes_per_second": benchmark["bytes_per_second"],
+"gpu_mem_occupancy": benchmark["gpu_mem_occupancy"]
+} for benchmark in data["benchmarks"]]
+
+
+
+
+plotter = lambda plt, x_name, y_name, data_dict_sorted, filter : plt.plot([item[x_name] for item in data_dict_sorted if filter(item)], [item[y_name] for item in data_dict_sorted if filter(item)], marker='o', markersize=5, label=f'nx={nx}')
+
 ########
 ## ny ##
 ########
 
-# Plotting the data for each group
+data_dict_sorted = sorted(data_dict, key=itemgetter("nx","ny"))
 plt.figure(figsize=(8, 6))
-for nx, group_data in data_groups["nx"].items():
-    ny = group_data["ny"]
-    throughput = [group_data["bytes_per_second"][i] for i in range(len(ny))]
-    plt.plot(ny, throughput, marker='o', markersize=5, label=f'nx={nx}')
+ 
+for nx in nx_values:
+	filter = lambda item : item["nx"]==nx and item["on_gpu"]
+	plotter(plt, "ny", "bytes_per_second", data_dict_sorted, filter)
 
-x = np.linspace(min(ny), 20*min(ny))
-plt.plot(x, np.mean([data_groups["nx"][nx]["bytes_per_second"][0] for nx in nx_values])/min(ny)*x, linestyle='--', color='black', label='perfect scaling')
+ny_min = min([item["ny"] for item in data_dict_sorted if item["on_gpu"]])
+x = np.linspace(ny_min, 20*ny_min)
+plt.plot(x, np.mean([item["bytes_per_second"] for item in data_dict_sorted if item["ny"]==ny_min and item["on_gpu"]])/ny_min*x, linestyle='--', color='black', label='perfect scaling')
 
-# Plotting the data
 plt.grid()
 plt.xscale("log")
 plt.xlabel("ny")
@@ -54,14 +71,16 @@ plt.title("Throughput on "+str.upper(data["context"]["chip"]));
 plt.legend()
 plt.savefig("throughput_ny.png")
 
-#gpu_mem
-plt.figure(figsize=(8, 6))
-for nx, group_data in data_groups["nx"].items():
-    ny = [group_data["ny"][i] for i in range(len(group_data["ny"])) if group_data["ny"][i]>=8e3]
-    gpu_mem_overhead = [(group_data["gpu_mem_occupancy"][i]-nx*group_data["ny"][i]*8)/(nx*group_data["ny"][i]*8)*100 for i in range(len(group_data["ny"])) if group_data["ny"][i]>=8e3]
-    plt.plot(ny, gpu_mem_overhead, marker='o', markersize=5, label=f'nx={nx}')
+#############
+## gpu_mem ##
+#############
 
-# Plotting the data
+plt.figure(figsize=(8, 6))
+
+for nx in nx_values:
+	filter = lambda item : item["nx"]==nx and item["on_gpu"] and item["ny"]>=8e3
+	plt.plot([item["ny"] for item in data_dict_sorted if filter(item)], [(item["gpu_mem_occupancy"]-nx*item["ny"]*8)/(nx*item["ny"]*8)*100 for item in data_dict_sorted if filter(item)], marker='o', markersize=5, label=f'nx={nx}')
+
 plt.grid()
 plt.xscale("log")
 plt.xlabel("ny")

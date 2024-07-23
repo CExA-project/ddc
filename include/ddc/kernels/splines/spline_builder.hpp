409d4c4aeb845f412b13a19709dbfa135e0acc8d
blegouix <stilynx51@gmail.com>
2024-07-23 13:43:57 +0200

    wip

--- include/ddc/kernels/splines/spline_builder.hpp
+++ include/ddc/kernels/splines/spline_builder.hpp
@@ -963,18 +963,14 @@ SplineBuilder<
             coefficients(ddc::DiscreteDomain<interpolation_discrete_dimension_type>(
                     interpolation_domain().front(),
                     ddc::DiscreteVector<interpolation_discrete_dimension_type>(
-                            bsplines_type::is_periodic()
-                                    ? interpolation_domain().size()
-                                    : ddc::discrete_space<bsplines_type>().nbasis())));
+                            ddc::discrete_space<bsplines_type>().size() - s_nbc_xmin
+                            - s_nbc_xmax)));
     Kokkos::deep_copy(
             coefficients.allocation_kokkos_view(),
             integral_bsplines_without_periodic_point
                     [ddc::DiscreteDomain<bsplines_type>(
-                             ddc::DiscreteElement<bsplines_type>(0),
-                             ddc::DiscreteVector<bsplines_type>(
-                                     bsplines_type::is_periodic()
-                                             ? interpolation_domain().size()
-                                             : ddc::discrete_space<bsplines_type>().nbasis()))]
+                             ddc::DiscreteElement<bsplines_type>(s_nbc_xmin),
+                             ddc::DiscreteVector<bsplines_type>(coefficients.size()))]
                             .allocation_kokkos_view());
 
     return coefficients;

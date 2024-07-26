c302fb4b0f91618a475c741edc6c95166931505e
tpadioleau <tpadioleau@noreply.example.com>
2024-07-26 11:32:10 +0000

    Update to match 6e88f6be50a34affb9afcc9ff6227c131f1c7d23 by push 2712

--- structddc_1_1reducer_1_1land.js
+++ structddc_1_1reducer_1_1land.js
@@ -1,5 +1,5 @@
 var structddc_1_1reducer_1_1land =
 [
-    [ "value_type", "structddc_1_1reducer_1_1land.html#a509635986a619922bfc25ae029afb695", null ],
-    [ "operator()", "structddc_1_1reducer_1_1land.html#a1dafa6b15fb6108113ff0c4cee620b86", null ]
+    [ "value_type", "structddc_1_1reducer_1_1land.html#a32733e5ad651b36dccc7243802cce3a2", null ],
+    [ "operator()", "structddc_1_1reducer_1_1land.html#a80490e289b6fedf06453be8f1b2b6d51", null ]
 ];
--- structddc_1_1reducer_1_1lor.js
+++ structddc_1_1reducer_1_1lor.js
@@ -1,5 +1,5 @@
 var structddc_1_1reducer_1_1lor =
 [
-    [ "value_type", "structddc_1_1reducer_1_1lor.html#add768797a8e868bc0a223cc2c2f8fd96", null ],
-    [ "operator()", "structddc_1_1reducer_1_1lor.html#a46c21862905e3c51e758907bcd204323", null ]
+    [ "value_type", "structddc_1_1reducer_1_1lor.html#a2a4a1a017019f131705656b77dcf537d", null ],
+    [ "operator()", "structddc_1_1reducer_1_1lor.html#ad55bf58dbee06048a5b7a80f88c6f715", null ]
 ];

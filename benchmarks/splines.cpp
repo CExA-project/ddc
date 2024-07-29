841870cb8eff1624d504d1ddb00e92b160c13dbf
Yuuichi Asahi <yuuichi.asahi@cea.fr>
2024-07-29 08:59:41 +0200

    update benchmarks

--- benchmarks/splines.cpp
+++ benchmarks/splines.cpp
@@ -16,30 +16,38 @@
 
 namespace DDC_HIP_5_7_ANONYMOUS_NAMESPACE_WORKAROUND(SPLINES_CPP)
 {
-    static constexpr std::size_t s_degree_x = 3;
-
     struct X
     {
         static constexpr bool PERIODIC = true;
     };
 
-    struct BSplinesX : ddc::UniformBSplines<X, s_degree_x>
+    template <typename NonUniform, std::size_t s_degree_x>
+    struct BSplinesX
+        : std::conditional_t<
+                  NonUniform::value,
+                  ddc::NonUniformBSplines<X, s_degree_x>,
+                  ddc::UniformBSplines<X, s_degree_x>>
     {
     };
+
+    template <typename NonUniform, std::size_t s_degree_x>
     using GrevillePoints = ddc::GrevilleInterpolationPoints<
-            BSplinesX,
+            BSplinesX<NonUniform, s_degree_x>,
             ddc::BoundCond::PERIODIC,
             ddc::BoundCond::PERIODIC>;
-    struct DDimX : GrevillePoints::interpolation_discrete_dimension_type
+
+    template <typename NonUniform, std::size_t s_degree_x>
+    struct DDimX : GrevillePoints<NonUniform, s_degree_x>::interpolation_discrete_dimension_type
     {
     };
 
     struct Y;
+
     struct DDimY : ddc::UniformPointSampling<Y>
     {
     };
 
-} // namespace )
+} // namespace DDC_HIP_5_7_ANONYMOUS_NAMESPACE_WORKAROUND(SPLINES_CPP)
 
 // Function to monitor GPU memory asynchronously
 void monitorMemoryAsync(std::mutex& mutex, bool& monitorFlag, size_t& maxUsedMem)
@@ -47,7 +55,7 @@ void monitorMemoryAsync(std::mutex& mutex, bool& monitorFlag, size_t& maxUsedMem
     size_t freeMem = 0;
     size_t totalMem = 0;
     while (monitorFlag) {
-        std::this_thread::sleep_for(std::chrono::milliseconds(1)); // Adjust the interval as needed
+        std::this_thread::sleep_for(std::chrono::milliseconds(10)); // Adjust the interval as needed
 
         // Acquire a lock to ensure thread safety when accessing CUDA functions
         std::lock_guard<std::mutex> lock(mutex);
@@ -59,8 +67,14 @@ void monitorMemoryAsync(std::mutex& mutex, bool& monitorFlag, size_t& maxUsedMem
     }
 }
 
+template <typename NonUniform, std::size_t s_degree_x>
 static void characteristics_advection(benchmark::State& state)
 {
+    std::size_t nx = state.range(0);
+    std::size_t ny = state.range(1);
+    int cols_per_chunk = state.range(2);
+    int preconditioner_max_block_size = state.range(3);
+
     size_t freeMem = 0;
     size_t totalMem = 0;
 #if defined(__CUDACC__)
@@ -80,32 +94,45 @@ static void characteristics_advection(benchmark::State& state)
             std::ref(monitorFlag),
             std::ref(maxUsedMem));
 
-    ddc::init_discrete_space<
-            BSplinesX>(ddc::Coordinate<X>(-1.), ddc::Coordinate<X>(1.), state.range(0));
-    ddc::init_discrete_space<DDimX>(ddc::GrevilleInterpolationPoints<
-                                    BSplinesX,
-                                    ddc::BoundCond::PERIODIC,
-                                    ddc::BoundCond::PERIODIC>::get_sampling<DDimX>());
+    if constexpr (!NonUniform::value) {
+        ddc::init_discrete_space<BSplinesX<
+                NonUniform,
+                s_degree_x>>(ddc::Coordinate<X>(0.), ddc::Coordinate<X>(1.), nx);
+    } else {
+        std::vector<ddc::Coordinate<X>> breaks(nx + 1);
+        for (std::size_t i(0); i < nx + 1; ++i) {
+            breaks[i] = ddc::Coordinate<X>(static_cast<double>(i) / nx);
+        }
+        ddc::init_discrete_space<BSplinesX<NonUniform, s_degree_x>>(breaks);
+    }
+
+    ddc::init_discrete_space<DDimX<NonUniform, s_degree_x>>(
+            ddc::GrevilleInterpolationPoints<
+                    BSplinesX<NonUniform, s_degree_x>,
+                    ddc::BoundCond::PERIODIC,
+                    ddc::BoundCond::PERIODIC>::
+                    template get_sampling<DDimX<NonUniform, s_degree_x>>());
     ddc::DiscreteDomain<DDimY> y_domain = ddc::init_discrete_space<DDimY>(DDimY::init<DDimY>(
             ddc::Coordinate<Y>(-1.),
             ddc::Coordinate<Y>(1.),
-            ddc::DiscreteVector<DDimY>(state.range(1))));
+            ddc::DiscreteVector<DDimY>(ny)));
 
     auto const x_domain = ddc::GrevilleInterpolationPoints<
-            BSplinesX,
+            BSplinesX<NonUniform, s_degree_x>,
             ddc::BoundCond::PERIODIC,
-            ddc::BoundCond::PERIODIC>::get_domain<DDimX>();
+            ddc::BoundCond::PERIODIC>::template get_domain<DDimX<NonUniform, s_degree_x>>();
     ddc::Chunk density_alloc(
-            ddc::DiscreteDomain<DDimX, DDimY>(x_domain, y_domain),
+            ddc::DiscreteDomain<DDimX<NonUniform, s_degree_x>, DDimY>(x_domain, y_domain),
             ddc::DeviceAllocator<double>());
     ddc::ChunkSpan const density = density_alloc.span_view();
     // Initialize the density on the main domain
-    ddc::DiscreteDomain<DDimX, DDimY> x_mesh
-            = ddc::DiscreteDomain<DDimX, DDimY>(x_domain, y_domain);
+    ddc::DiscreteDomain<DDimX<NonUniform, s_degree_x>, DDimY> x_mesh
+            = ddc::DiscreteDomain<DDimX<NonUniform, s_degree_x>, DDimY>(x_domain, y_domain);
     ddc::parallel_for_each(
+            Kokkos::DefaultExecutionSpace(),
             x_mesh,
-            KOKKOS_LAMBDA(ddc::DiscreteElement<DDimX, DDimY> const ixy) {
-                double const x = ddc::coordinate(ddc::select<DDimX>(ixy));
+            KOKKOS_LAMBDA(ddc::DiscreteElement<DDimX<NonUniform, s_degree_x>, DDimY> const ixy) {
+                double const x = ddc::coordinate(ddc::select<DDimX<NonUniform, s_degree_x>>(ixy));
                 double const y = ddc::coordinate(ddc::select<DDimY>(ixy));
                 density(ixy) = 9.999 * Kokkos::exp(-(x * x + y * y) / 0.1 / 2);
                 // initial_density(ixy) = 9.999 * ((x * x + y * y) < 0.25);
@@ -113,8 +140,8 @@ static void characteristics_advection(benchmark::State& state)
     ddc::SplineBuilder<
             Kokkos::DefaultExecutionSpace,
             Kokkos::DefaultExecutionSpace::memory_space,
-            BSplinesX,
-            DDimX,
+            BSplinesX<NonUniform, s_degree_x>,
+            DDimX<NonUniform, s_degree_x>,
             ddc::BoundCond::PERIODIC,
             ddc::BoundCond::PERIODIC,
 #if defined(SOLVER_LAPACK)
@@ -122,18 +149,18 @@ static void characteristics_advection(benchmark::State& state)
 #elif defined(SOLVER_GINKGO)
             ddc::SplineSolver::GINKGO,
 #endif
-            DDimX,
+            DDimX<NonUniform, s_degree_x>,
             DDimY>
-            spline_builder(x_mesh, state.range(2), state.range(3));
+            spline_builder(x_mesh, cols_per_chunk, preconditioner_max_block_size);
     ddc::PeriodicExtrapolationRule<X> periodic_extrapolation;
     ddc::SplineEvaluator<
             Kokkos::DefaultExecutionSpace,
             Kokkos::DefaultExecutionSpace::memory_space,
-            BSplinesX,
-            DDimX,
+            BSplinesX<NonUniform, s_degree_x>,
+            DDimX<NonUniform, s_degree_x>,
             ddc::PeriodicExtrapolationRule<X>,
             ddc::PeriodicExtrapolationRule<X>,
-            DDimX,
+            DDimX<NonUniform, s_degree_x>,
             DDimY>
             spline_evaluator(periodic_extrapolation, periodic_extrapolation);
     ddc::Chunk coef_alloc(
@@ -150,9 +177,10 @@ static void characteristics_advection(benchmark::State& state)
     for (auto _ : state) {
         Kokkos::Profiling::pushRegion("FeetCharacteristics");
         ddc::parallel_for_each(
+                Kokkos::DefaultExecutionSpace(),
                 feet_coords.domain(),
-                KOKKOS_LAMBDA(ddc::DiscreteElement<DDimX, DDimY> const e) {
-                    feet_coords(e) = ddc::coordinate(ddc::select<DDimX>(e))
+                KOKKOS_LAMBDA(ddc::DiscreteElement<DDimX<NonUniform, s_degree_x>, DDimY> const e) {
+                    feet_coords(e) = ddc::coordinate(ddc::select<DDimX<NonUniform, s_degree_x>>(e))
                                      - ddc::Coordinate<X>(0.0176429863);
                 });
         Kokkos::Profiling::popRegion();
@@ -166,9 +194,7 @@ static void characteristics_advection(benchmark::State& state)
     }
     monitorFlag = false;
     monitorThread.join();
-    state.SetBytesProcessed(
-            int64_t(state.iterations())
-            * int64_t(state.range(0) * state.range(1) * sizeof(double)));
+    state.SetBytesProcessed(int64_t(state.iterations()) * int64_t(nx * ny * sizeof(double)));
     state.counters["gpu_mem_occupancy"] = maxUsedMem - initUsedMem;
     ////////////////////////////////////////////////////
     /// --------------- HUGE WARNING --------------- ///
@@ -178,9 +204,13 @@ static void characteristics_advection(benchmark::State& state)
     /// The reason is it acts on underlying global   ///
     /// variables, which is always a bad idea.       ///
     ////////////////////////////////////////////////////
-    ddc::detail::g_discrete_space_dual<BSplinesX>.reset();
-    ddc::detail::g_discrete_space_dual<ddc::UniformBsplinesKnots<BSplinesX>>.reset();
-    ddc::detail::g_discrete_space_dual<DDimX>.reset();
+    ddc::detail::g_discrete_space_dual<BSplinesX<NonUniform, s_degree_x>>.reset();
+    if constexpr (!NonUniform::value) {
+        ddc::detail::g_discrete_space_dual<ddc::UniformBsplinesKnots<BSplinesX<NonUniform, s_degree_x>>>.reset();
+    } else {
+        ddc::detail::g_discrete_space_dual<ddc::NonUniformBsplinesKnots<BSplinesX<NonUniform, s_degree_x>>>.reset();
+    }
+    ddc::detail::g_discrete_space_dual<DDimX<NonUniform, s_degree_x>>.reset();
     ddc::detail::g_discrete_space_dual<DDimY>.reset();
     ////////////////////////////////////////////////////
 }
@@ -201,32 +231,72 @@ std::size_t cols_per_chunk_ref = 8192;
 unsigned int preconditioner_max_block_size_ref = 32u;
 #endif
 
-BENCHMARK(characteristics_advection)
+// Uniform mesh degree 3-5
+BENCHMARK(characteristics_advection<std::false_type, 3>)
         ->RangeMultiplier(2)
-        ->Ranges(
-                {{64, 1024},
-                 {100, 200000},
-                 {cols_per_chunk_ref, cols_per_chunk_ref},
-                 {preconditioner_max_block_size_ref, preconditioner_max_block_size_ref}})
+        ->Ranges({{64, 1024},
+                  {100, 200000},
+                  {cols_per_chunk_ref, cols_per_chunk_ref},
+                  {preconditioner_max_block_size_ref, preconditioner_max_block_size_ref}})
         ->MinTime(3)
         ->UseRealTime();
-/*
-BENCHMARK(characteristics_advection)
+
+BENCHMARK(characteristics_advection<std::false_type, 4>)
         ->RangeMultiplier(2)
-        ->Ranges({{64, 1024}, {100000, 100000}, {64,65535}, {preconditioner_max_block_size_ref, preconditioner_max_block_size_ref}})
-        ->MinTime(3)->UseRealTime();
-*/
-/*
-BENCHMARK(characteristics_advection)
+        ->Ranges({{64, 1024},
+                  {100, 200000},
+                  {cols_per_chunk_ref, cols_per_chunk_ref},
+                  {preconditioner_max_block_size_ref, preconditioner_max_block_size_ref}})
+        ->MinTime(3)
+        ->UseRealTime();
+
+BENCHMARK(characteristics_advection<std::false_type, 5>)
         ->RangeMultiplier(2)
-        ->Ranges({{64, 1024}, {100000, 100000}, {cols_per_chunk_ref, cols_per_chunk_ref}, {1, 32}})
-        ->MinTime(3)->UseRealTime();
-*/
+        ->Ranges({{64, 1024},
+                  {100, 200000},
+                  {cols_per_chunk_ref, cols_per_chunk_ref},
+                  {preconditioner_max_block_size_ref, preconditioner_max_block_size_ref}})
+        ->MinTime(3)
+        ->UseRealTime();
+
+// Uniform mesh degree 3-5
+BENCHMARK(characteristics_advection<std::true_type, 3>)
+        ->RangeMultiplier(2)
+        ->Ranges({{64, 1024},
+                  {100, 200000},
+                  {cols_per_chunk_ref, cols_per_chunk_ref},
+                  {preconditioner_max_block_size_ref, preconditioner_max_block_size_ref}})
+        ->MinTime(3)
+        ->UseRealTime();
+
+BENCHMARK(characteristics_advection<std::true_type, 4>)
+        ->RangeMultiplier(2)
+        ->Ranges({{64, 1024},
+                  {100, 200000},
+                  {cols_per_chunk_ref, cols_per_chunk_ref},
+                  {preconditioner_max_block_size_ref, preconditioner_max_block_size_ref}})
+        ->MinTime(3)
+        ->UseRealTime();
+
+BENCHMARK(characteristics_advection<std::true_type, 5>)
+        ->RangeMultiplier(2)
+        ->Ranges({{64, 1024},
+                  {100, 200000},
+                  {cols_per_chunk_ref, cols_per_chunk_ref},
+                  {preconditioner_max_block_size_ref, preconditioner_max_block_size_ref}})
+        ->MinTime(3)
+        ->UseRealTime();
 
 int main(int argc, char** argv)
 {
     ::benchmark::Initialize(&argc, argv);
     ::benchmark::AddCustomContext("chip", chip);
+#if defined(SOLVER_LAPACK)
+    std::string backend = "LAPACK";
+#elif defined(SOLVER_GINKGO)
+    std::string backend = "GINKGO";
+#endif
+    ::benchmark::AddCustomContext("backend", backend);
     ::benchmark::AddCustomContext("cols_per_chunk_ref", std::to_string(cols_per_chunk_ref));
     ::benchmark::AddCustomContext(
             "preconditioner_max_block_size_ref",

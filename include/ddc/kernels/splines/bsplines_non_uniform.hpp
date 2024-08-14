94ecf6c215f2257dc876078464e0f4d13db7f496
Emily Bourne <emily.bourne@epfl.ch>
2024-08-14 15:23:21 +0200

    Fix assertion

--- include/ddc/kernels/splines/bsplines_non_uniform.hpp
+++ include/ddc/kernels/splines/bsplines_non_uniform.hpp
@@ -475,7 +475,7 @@ KOKKOS_INLINE_FUNCTION ddc::DiscreteElement<DDim> NonUniformBSplines<CDim, D>::
     assert(icell >= m_break_point_domain.front());
     assert(icell <= m_break_point_domain.back());
     assert(ddc::coordinate(icell) - x <= 1e-14);
-    assert(ddc::coordinate(icell + 1) - x >= 1e-14);
+    assert(x - ddc::coordinate(icell + 1) <= 1e-14);
 
     // 2. Compute values of B-splines with support over cell 'icell'
     double temp;

7233e7c21fac6c3616eaa90e00b1a8b461059f95
tpadioleau <tpadioleau@noreply.example.com>
2024-09-03 11:45:39 +0000

    Update to match 866e187defa384263e000917f12b4ea570ef1afc by push 2920

--- search/variables_4.js
+++ search/variables_4.js
@@ -4,5 +4,6 @@ var searchData=
   ['s_5fbc_5fxmin_1',['s_bc_xmin',['../classddc_1_1SplineBuilder.html#a8d9d5a219b1ecb2fe053073449576586',1,'ddc::SplineBuilder']]],
   ['s_5fnbc_5fxmax_2',['s_nbc_xmax',['../classddc_1_1SplineBuilder.html#a35c679bf391a99e38965721da44d1682',1,'ddc::SplineBuilder']]],
   ['s_5fnbc_5fxmin_3',['s_nbc_xmin',['../classddc_1_1SplineBuilder.html#aa219e8b4470dc5ff80c8ae62fa8f07b5',1,'ddc::SplineBuilder']]],
-  ['s_5fodd_4',['s_odd',['../classddc_1_1SplineBuilder.html#a1d5156d53853ead1dd8260ba37e7cb43',1,'ddc::SplineBuilder']]]
+  ['s_5fodd_4',['s_odd',['../classddc_1_1SplineBuilder.html#a1d5156d53853ead1dd8260ba37e7cb43',1,'ddc::SplineBuilder']]],
+  ['s_5fspline_5fsolver_5',['s_spline_solver',['../classddc_1_1SplineBuilder.html#ae0d1470f62c53e46dadc28fc8e5ef28c',1,'ddc::SplineBuilder']]]
 ];

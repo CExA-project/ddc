a4732dcb302993bb46472f2095ca172b6efa2b6d
Thomas Padioleau <thomas.padioleau@cea.fr>
2024-09-04 11:23:04 +0200

    Forbid to construct a ChunkSpan from a rvalue of type Chunk (#619)
    * Forbid to construct a ChunkSpan from a rvalue of type Chunk
    
    * Remove pulling

---
+++ tests/chunk_span.cpp
@@ -0,0 +1,35 @@
+// Copyright (C) The DDC development team, see COPYRIGHT.md file
+//
+// SPDX-License-Identifier: MIT
+
+#include <type_traits>
+
+#include <ddc/ddc.hpp>
+
+#include <gtest/gtest.h>
+
+namespace DDC_HIP_5_7_ANONYMOUS_NAMESPACE_WORKAROUND(CHUNK_SPAN_CPP)
+{
+    struct DDimX
+    {
+    };
+    using DElemX = ddc::DiscreteElement<DDimX>;
+    using DVectX = ddc::DiscreteVector<DDimX>;
+    using DDomX = ddc::DiscreteDomain<DDimX>;
+
+    template <class Datatype>
+    using ChunkX = ddc::Chunk<Datatype, DDomX>;
+
+    template <class Datatype>
+    using ChunkSpanX = ddc::ChunkSpan<Datatype, DDomX>;
+
+} // namespace )
+
+TEST(ChunkSpan1DTest, ConstructionFromChunk)
+{
+    EXPECT_FALSE((std::is_constructible_v<ChunkSpanX<double>, ChunkX<double>>));
+    EXPECT_FALSE((std::is_constructible_v<ChunkSpanX<double>, ChunkX<double> const>));
+    EXPECT_TRUE((std::is_constructible_v<ChunkSpanX<double>, ChunkX<double>&>));
+    EXPECT_TRUE((std::is_constructible_v<ChunkSpanX<const double>, ChunkX<double>&>));
+    EXPECT_TRUE((std::is_constructible_v<ChunkSpanX<const double>, ChunkX<double> const&>));
+}

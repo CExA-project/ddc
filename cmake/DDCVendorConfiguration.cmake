579427b92e6246a5c4e67a4bc16747e9f2624a96
Thomas Padioleau <thomas.padioleau@cea.fr>
2024-10-17 16:39:58 +0200

    Avoid redundant cmake logic (#660)
    * Refactor our cmake logic
    
    * Add missing dependencies on installed config

---
+++ cmake/DDCVendorConfiguration.cmake
@@ -0,0 +1,42 @@
+# Copyright (C) The DDC development team, see COPYRIGHT.md file
+#
+# SPDX-License-Identifier: MIT
+
+# This file provides different macros that will configure the dependencies with required options.
+# They should be used only if DDC is responsible of handling the vendor dependency.
+
+macro(ddc_configure_benchmark)
+    option(BENCHMARK_ENABLE_TESTING "Enable testing of the benchmark library." OFF)
+    option(BENCHMARK_ENABLE_INSTALL "Enable installation of benchmark. (Projects embedding benchmark may want to turn this OFF.)" OFF)
+    add_subdirectory(vendor/benchmark)
+endmacro()
+
+macro(ddc_configure_googletest)
+    add_subdirectory(vendor/googletest)
+endmacro()
+
+macro(ddc_configure_kokkos)
+    if("${Kokkos_ENABLE_CUDA}")
+        option(Kokkos_ENABLE_CUDA_CONSTEXPR "Whether to activate experimental relaxed constexpr functions" ON)
+        option(Kokkos_ENABLE_CUDA_RELOCATABLE_DEVICE_CODE "Whether to enable relocatable device code (RDC) for CUDA" ON)
+    endif()
+    if("${Kokkos_ENABLE_HIP}")
+        option(Kokkos_ENABLE_HIP_RELOCATABLE_DEVICE_CODE "Whether to enable relocatable device code (RDC) for HIP" ON)
+    endif()
+    add_subdirectory(vendor/kokkos)
+endmacro()
+
+macro(ddc_configure_kokkos_fft)
+    option(KokkosFFT_ENABLE_HOST_AND_DEVICE "Enable FFT on both host and device" ON)
+    add_subdirectory(vendor/kokkos-fft)
+endmacro()
+
+macro(ddc_configure_kokkos_kernels)
+    set(KokkosKernels_ENABLE_ALL_COMPONENTS OFF)
+    set(KokkosKernels_ENABLE_COMPONENT_BLAS ON)
+    set(KokkosKernels_ENABLE_COMPONENT_BATCHED ON)
+    set(KokkosKernels_ENABLE_COMPONENT_LAPACK OFF)
+    set(KokkosKernels_ENABLE_TPL_BLAS OFF)
+    set(KokkosKernels_ENABLE_TPL_LAPACK OFF)
+    add_subdirectory(vendor/kokkos-kernels)
+endmacro()

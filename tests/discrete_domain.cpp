f54bba905ab0ed01c2a23f2d62d5b3c88db41f04
Thomas Padioleau <thomas.padioleau@cea.fr>
2024-12-05 22:21:11 +0100

    Transfer distance function in DiscreteDomain

--- tests/discrete_domain.cpp
+++ tests/discrete_domain.cpp
@@ -245,6 +245,19 @@ TEST(DiscreteDomainTest, Remove)
             DDomXY(dom_x_y.front() + DVectXY(1, 4), dom_x_y.extents() - DVectXY(2, 5)));
 }
 
+TEST(DiscreteDomainTest, IsInside)
+{
+    DDomXY const dom_x_y(lbound_x_y, nelems_x_y);
+    EXPECT_TRUE(dom_x_y.is_inside(lbound_x_y));
+    EXPECT_FALSE(dom_x_y.is_inside(lbound_x_y + nelems_x_y));
+}
+
+TEST(DiscreteDomainTest, DistanceFromFront)
+{
+    DDomXY const dom_x_y(lbound_x_y, nelems_x_y);
+    EXPECT_EQ(dom_x_y.distance_from_front(lbound_x_y), DVectXY(0, 0));
+}
+
 TEST(DiscreteDomainTest, SliceDomainXTooearly)
 {
 #ifndef NDEBUG // The assertion is only checked if NDEBUG isn't defined

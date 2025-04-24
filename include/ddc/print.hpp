508f05bf45f4913d8e31c1d21ff94aa05542e6ac
PaulGannay <paul.gannay@cea.fr>
2025-04-24 13:28:04 +0200

    Function to print ChunkSpan for debugging purpose (#819)
    * First draft for chunk printing
    
    * Spell check correction
    
    * Take review comment into account
    
    Correctly manage chunk that are not accessible from the host.
    Correctly manage 0d chunk.
    Correct tests
    
    * Remove unused parameter
    
    * run clang-format
    
    * Use std::free for hip
    
    * Correct errors reported by clang-tidy
    
    * Correct mdpsan access for C++23
    
    * Correct ambiguous template with older version of clang
    
    * msvc format display of float in hex mode is different from gcc and clang
    
    (it looks like msvc is doing a conversion to double before converting to
    hex)
    
    * Correctly execute test on device
    
    * Formatting
    
    * Fix GTest target
    
    * Cleanup headers
    
    * Enforce east const
    
    * Use single character
    
    * Correct the regular expression used to check the view metadata
    
    * Use parallel_fill when possible
    
    * Take PR comment into account
    
    * spell check
    
    * Clang format with correct clang version
    
    * Const eastness
    
    * s_ prefix for static variables
    
    * Reorder list of author and added myself
    
    ---------
    
    Co-authored-by: Thomas Padioleau <thomas.padioleau@cea.fr>

---
+++ include/ddc/print.hpp
@@ -0,0 +1,370 @@
+// Copyright (C) The DDC development team, see COPYRIGHT.md file
+//
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+#include <algorithm>
+#include <cstddef>
+#include <memory>
+#include <ostream>
+#include <sstream>
+#include <type_traits>
+#include <utility>
+
+#include "ddc/chunk_span.hpp"
+#include "ddc/discrete_domain.hpp"
+
+#if defined(KOKKOS_COMPILER_GNU) || defined(KOKKOS_COMPILER_CLANG)
+#    include <cxxabi.h>
+#endif
+
+namespace ddc {
+namespace detail {
+class ChunkPrinter
+{
+    static constexpr int const s_threshold = 10;
+    // If this ever becomes modifiable by the user, we need to ensure that
+    // s_edgeitems < (s_threshold / 2) stays true.
+    static constexpr int const s_edgeitems = 3;
+
+    std::stringstream m_ss;
+
+    static std::ostream& alignment(std::ostream& os, int level)
+    {
+        for (int i = 0; i <= level; ++i) {
+            os << ' ';
+        }
+        return os;
+    }
+
+    template <class T>
+    std::size_t get_element_width(T const& elem)
+    {
+        m_ss.seekp(0);
+        m_ss << elem;
+        return m_ss.tellp();
+    }
+
+    template <class T>
+    void display_aligned_element(std::ostream& os, T const& elem, std::size_t largest_element)
+    {
+        std::size_t const elem_width = get_element_width(elem);
+
+        for (std::size_t i = 0; i < largest_element - elem_width; ++i) {
+            os << " ";
+        }
+        os << elem;
+    }
+
+    template <class ElementType, class Extents, class Layout, class Accessor>
+    std::ostream& base_case_display(
+            std::ostream& os,
+            Kokkos::mdspan<ElementType, Extents, Layout, Accessor> const& s,
+            std::size_t largest_element,
+            std::size_t beginning,
+            std::size_t end,
+            std::size_t extent)
+    {
+        for (std::size_t i0 = beginning; i0 < end; ++i0) {
+            display_aligned_element(os, s[i0], largest_element);
+            if (i0 < extent - 1) {
+                os << " ";
+            }
+        }
+        return os;
+    }
+
+    template <class ElementType, class Extents, class Layout, class Accessor, std::size_t... Is>
+    std::ostream& recursive_display(
+            std::ostream& os,
+            Kokkos::mdspan<ElementType, Extents, Layout, Accessor> const& s,
+            int level,
+            std::size_t largest_element,
+            std::size_t beginning,
+            std::size_t end,
+            std::index_sequence<Is...>)
+    {
+        for (std::size_t i0 = beginning; i0 < end; ++i0) {
+            print_impl(
+                    os,
+                    Kokkos::submdspan(s, i0, ((void)Is, Kokkos::full_extent)...),
+                    level + 1,
+                    largest_element,
+                    std::make_index_sequence<sizeof...(Is)>());
+            if (i0 < end - 1) {
+                for (int ndims = 0; ndims < sizeof...(Is); ++ndims) {
+                    os << '\n';
+                }
+                alignment(os, level);
+            }
+        }
+
+        return os;
+    }
+
+public:
+    // 0D chunk span
+    template <class ElementType, class Extents, class Layout, class Accessor>
+    std::ostream& print_impl(
+            std::ostream& os,
+            Kokkos::mdspan<ElementType, Extents, Layout, Accessor> const& s,
+            int /*level*/,
+            std::size_t /*largest_element*/,
+            std::index_sequence<>)
+    {
+        return os << *s.data_handle();
+    }
+
+    // Recursively parse the chunk to print it
+    template <
+            class ElementType,
+            class Extents,
+            class Layout,
+            class Accessor,
+            std::size_t I0,
+            std::size_t... Is>
+    std::ostream& print_impl(
+            std::ostream& os,
+            Kokkos::mdspan<ElementType, Extents, Layout, Accessor> const& s,
+            int level,
+            std::size_t largest_element,
+            std::index_sequence<I0, Is...>)
+    {
+        auto extent = s.extent(I0);
+        if constexpr (sizeof...(Is) > 0) {
+            os << '[';
+            if (extent < s_threshold) {
+                recursive_display(
+                        os,
+                        s,
+                        level,
+                        largest_element,
+                        0,
+                        extent,
+                        std::make_index_sequence<sizeof...(Is)>());
+            } else {
+                recursive_display(
+                        os,
+                        s,
+                        level,
+                        largest_element,
+                        0,
+                        s_edgeitems,
+                        std::make_index_sequence<sizeof...(Is)>());
+                for (int ndims = 0; ndims < sizeof...(Is); ++ndims) {
+                    os << '\n';
+                }
+                alignment(os, level);
+                os << "...";
+                for (int ndims = 0; ndims < sizeof...(Is); ++ndims) {
+                    os << '\n';
+                }
+                alignment(os, level);
+                recursive_display(
+                        os,
+                        s,
+                        level,
+                        largest_element,
+                        extent - s_edgeitems,
+                        extent,
+                        std::make_index_sequence<sizeof...(Is)>());
+            }
+            os << "]";
+        } else {
+            os << "[";
+            if (extent < s_threshold) {
+                base_case_display(os, s, largest_element, 0, extent, extent);
+            } else {
+                base_case_display(os, s, largest_element, 0, s_edgeitems, extent);
+                os << "... ";
+                base_case_display(os, s, largest_element, extent - s_edgeitems, extent, extent);
+            }
+            os << "]";
+        }
+
+        return os;
+    }
+
+    // 0D, we don't need the element size in this case so the actual returned value can be anything.
+    template <class ElementType, class Extents, class Layout, class Accessor>
+    std::size_t find_largest_displayed_element(
+            Kokkos::mdspan<ElementType, Extents, Layout, Accessor> const&,
+            std::index_sequence<>)
+    {
+        return 0;
+    }
+
+    // Find the largest element we have to print to allow alignment (it ignore
+    // element that will be elided).
+    template <
+            class ElementType,
+            class Extents,
+            class Layout,
+            class Accessor,
+            std::size_t I0,
+            std::size_t... Is>
+    std::size_t find_largest_displayed_element(
+            Kokkos::mdspan<ElementType, Extents, Layout, Accessor> const& s,
+            std::index_sequence<I0, Is...>)
+    {
+        std::size_t ret = 0;
+        auto extent = s.extent(I0);
+        if constexpr (sizeof...(Is) > 0) {
+            if (extent < s_threshold) {
+                for (std::size_t i0 = 0; i0 < extent; ++i0) {
+                    ret = std::max(
+                            ret,
+                            find_largest_displayed_element(
+                                    Kokkos::submdspan(s, i0, ((void)Is, Kokkos::full_extent)...),
+                                    std::make_index_sequence<sizeof...(Is)>()));
+                }
+            } else {
+                for (std::size_t i0 = 0; i0 < s_edgeitems; ++i0) {
+                    ret = std::max(
+                            ret,
+                            find_largest_displayed_element(
+                                    Kokkos::submdspan(s, i0, ((void)Is, Kokkos::full_extent)...),
+                                    std::make_index_sequence<sizeof...(Is)>()));
+                }
+                for (std::size_t i0 = extent - s_edgeitems; i0 < extent; ++i0) {
+                    ret = std::max(
+                            ret,
+                            find_largest_displayed_element(
+                                    Kokkos::submdspan(s, i0, ((void)Is, Kokkos::full_extent)...),
+                                    std::make_index_sequence<sizeof...(Is)>()));
+                }
+            }
+        } else {
+            if (extent < s_threshold) {
+                for (std::size_t i0 = 0; i0 < extent; ++i0) {
+                    ret = std::max(ret, get_element_width(s[i0]));
+                }
+            } else {
+                for (std::size_t i0 = 0; i0 < s_edgeitems; ++i0) {
+                    ret = std::max(ret, get_element_width(s[i0]));
+                }
+                for (std::size_t i0 = extent - s_edgeitems; i0 < extent; ++i0) {
+                    ret = std::max(ret, get_element_width(s[i0]));
+                }
+            }
+        }
+
+        return ret;
+    }
+
+    explicit ChunkPrinter(std::ostream const& os)
+    {
+        m_ss.copyfmt(os);
+    }
+};
+
+#if defined(KOKKOS_COMPILER_GNU) || defined(KOKKOS_COMPILER_CLANG)
+template <class Type>
+void print_demangled_type_name(std::ostream& os)
+{
+    int status;
+
+    std::unique_ptr<char, decltype(std::free)*> const demangled_name(
+            abi::__cxa_demangle(typeid(Type).name(), nullptr, nullptr, &status),
+            std::free);
+    if (status != 0) {
+        os << "Error demangling dimension name: " << status;
+        return;
+    }
+
+    os << demangled_name.get();
+}
+#else
+template <class Type>
+void print_demangled_type_name(std::ostream& os)
+{
+    os << typeid(Type).name();
+}
+#endif
+
+inline void print_dim_name(std::ostream& os, DiscreteDomain<> const)
+{
+    os << "Scalar";
+}
+
+template <class Dim>
+void print_dim_name(std::ostream& os, DiscreteDomain<Dim> const dd)
+{
+    print_demangled_type_name<Dim>(os);
+    os << '(' << dd.size() << ')';
+}
+
+template <class Dim0, class Dim1, class... Dims>
+void print_dim_name(std::ostream& os, DiscreteDomain<Dim0, Dim1, Dims...> const dd)
+{
+    print_demangled_type_name<Dim0>(os);
+    DiscreteDomain<Dim1, Dims...> const smaller_dd(dd);
+    os << '(' << dd.size() / smaller_dd.size() << ")×";
+    print_dim_name(os, smaller_dd);
+}
+
+} // namespace detail
+
+template <class ElementType, class SupportType, class LayoutStridedPolicy, class MemorySpace>
+std::ostream& print_content(
+        std::ostream& os,
+        ChunkSpan<ElementType, SupportType, LayoutStridedPolicy, MemorySpace> const& chunk_span)
+{
+    auto h_chunk_span = create_mirror_view_and_copy(Kokkos::HostSpace(), chunk_span);
+
+    using chunkspan_type = std::remove_cv_t<std::remove_reference_t<decltype(h_chunk_span)>>;
+    using mdspan_type = typename chunkspan_type::allocation_mdspan_type;
+    using extents = typename mdspan_type::extents_type;
+
+    mdspan_type const allocated_mdspan = h_chunk_span.allocation_mdspan();
+
+    ddc::detail::ChunkPrinter printer(os);
+    std::size_t const largest_element = printer.find_largest_displayed_element(
+            allocated_mdspan,
+            std::make_index_sequence<extents::rank()>());
+
+    printer.print_impl(
+            os,
+            allocated_mdspan,
+            0,
+            largest_element,
+            std::make_index_sequence<extents::rank()>());
+
+    return os;
+}
+
+template <class ElementType, class SupportType, class LayoutStridedPolicy, class MemorySpace>
+std::ostream& print_type_info(
+        std::ostream& os,
+        ChunkSpan<ElementType, SupportType, LayoutStridedPolicy, MemorySpace> const& chunk_span)
+{
+    ddc::detail::print_dim_name(os, chunk_span.domain());
+    os << '\n';
+    ddc::detail::print_demangled_type_name<decltype(chunk_span)>(os);
+    os << '\n';
+
+    return os;
+}
+
+template <class ElementType, class SupportType, class LayoutStridedPolicy, class MemorySpace>
+std::ostream& print(
+        std::ostream& os,
+        ChunkSpan<ElementType, SupportType, LayoutStridedPolicy, MemorySpace> const& chunk_span)
+{
+    print_type_info(os, chunk_span);
+    print_content(os, chunk_span);
+
+    return os;
+}
+
+
+template <class ElementType, class SupportType, class LayoutStridedPolicy, class MemorySpace>
+std::ostream& operator<<(
+        std::ostream& os,
+        ChunkSpan<ElementType, SupportType, LayoutStridedPolicy, MemorySpace> const& chunk_span)
+{
+    return print(os, chunk_span);
+}
+
+} // namespace ddc
---
+++ tests/print.cpp
@@ -0,0 +1,284 @@
+// Copyright (C) The DDC development team, see COPYRIGHT.md file
+//
+// SPDX-License-Identifier: MIT
+
+#include <iomanip>
+#include <ios>
+#include <sstream>
+#include <string>
+#include <type_traits>
+
+#include <ddc/ddc.hpp>
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include <Kokkos_Core.hpp>
+
+inline namespace anonymous_namespace_workaround_print_cpp {
+
+struct Dim0
+{
+};
+struct Dim1
+{
+};
+struct Dim2
+{
+};
+struct Dim3
+{
+};
+struct Dim4
+{
+};
+struct Dim5
+{
+};
+
+} // namespace anonymous_namespace_workaround_print_cpp
+
+template <typename ElementType>
+void PrintTestCheckOutput0d()
+{
+    ddc::DiscreteDomain<> const domain_full;
+
+    ddc::Chunk chunk("chunk", domain_full, ddc::DeviceAllocator<ElementType>());
+    ddc::ChunkSpan const chunk_span = chunk.span_view();
+
+    ddc::parallel_fill(chunk_span, 0.12345);
+
+    {
+        std::stringstream ss;
+        print_content(ss, chunk_span);
+        EXPECT_EQ(ss.str(), "0.12345");
+    }
+}
+
+TEST(Print, CheckOutput0d)
+{
+    PrintTestCheckOutput0d<float>();
+    PrintTestCheckOutput0d<double>();
+}
+
+template <typename ElementType>
+void TestPrintCheckOutput2d()
+{
+    unsigned const dim0 = 2;
+    unsigned const dim1 = 2;
+
+    ddc::DiscreteDomain<Dim0> const domain_0
+            = ddc::init_trivial_bounded_space(ddc::DiscreteVector<Dim0>(dim0));
+    ddc::DiscreteDomain<Dim1> const domain_1
+            = ddc::init_trivial_bounded_space(ddc::DiscreteVector<Dim1>(dim1));
+
+    ddc::DiscreteDomain<Dim0, Dim1> const domain_2d(domain_0, domain_1);
+
+    ddc::Chunk chunk("chunk", domain_2d, ddc::DeviceAllocator<ElementType>());
+    ddc::ChunkSpan const chunk_span = chunk.span_view();
+
+    ddc::parallel_for_each(
+            domain_2d,
+            KOKKOS_LAMBDA(ddc::DiscreteElement<Dim0, Dim1> const i) {
+                if (i == domain_2d.front() + ddc::DiscreteVector<Dim0, Dim1>(1, 1)) {
+                    chunk_span(i) = -0.12345;
+                } else {
+                    chunk_span(i) = 0.12345;
+                }
+            });
+
+    {
+        std::stringstream ss;
+        print_content(ss, chunk_span);
+        EXPECT_EQ(
+                ss.str(),
+                "[[ 0.12345  0.12345]\n"
+                " [ 0.12345 -0.12345]]");
+    }
+    {
+        std::stringstream ss;
+        ss << std::setprecision(2);
+        print_content(ss, chunk_span);
+        EXPECT_EQ(
+                ss.str(),
+                "[[ 0.12  0.12]\n"
+                " [ 0.12 -0.12]]");
+    }
+    {
+        std::stringstream ss;
+        ss << std::hexfloat;
+        print_content(ss, chunk_span);
+        if constexpr (std::is_same_v<ElementType, double>) {
+            EXPECT_EQ(
+                    ss.str(),
+                    "[[ 0x1.f9a6b50b0f27cp-4  0x1.f9a6b50b0f27cp-4]\n"
+                    " [ 0x1.f9a6b50b0f27cp-4 -0x1.f9a6b50b0f27cp-4]]");
+        } else {
+#if defined(KOKKOS_COMPILER_MSVC)
+            EXPECT_EQ(
+                    ss.str(),
+                    "[[ 0x1.f9a6b60000000p-4  0x1.f9a6b60000000p-4]\n"
+                    " [ 0x1.f9a6b60000000p-4 -0x1.f9a6b60000000p-4]]");
+#else
+            EXPECT_EQ(
+                    ss.str(),
+                    "[[ 0x1.f9a6b6p-4  0x1.f9a6b6p-4]\n"
+                    " [ 0x1.f9a6b6p-4 -0x1.f9a6b6p-4]]");
+#endif
+        }
+    }
+    {
+        std::stringstream ss;
+        ss << std::scientific;
+        print_content(ss, chunk_span);
+        EXPECT_EQ(
+                ss.str(),
+                "[[ 1.234500e-01  1.234500e-01]\n"
+                " [ 1.234500e-01 -1.234500e-01]]");
+    }
+}
+
+TEST(Print, CheckOutput2d)
+{
+    TestPrintCheckOutput2d<float>();
+    TestPrintCheckOutput2d<double>();
+}
+
+template <typename ElementType>
+void TestPrintCheckoutOutput2dElision()
+{
+    unsigned const dim0 = 100;
+    unsigned const dim1 = 100;
+
+    ddc::DiscreteDomain<Dim0> const domain_0
+            = ddc::init_trivial_bounded_space(ddc::DiscreteVector<Dim0>(dim0));
+    ddc::DiscreteDomain<Dim1> const domain_1
+            = ddc::init_trivial_bounded_space(ddc::DiscreteVector<Dim1>(dim1));
+
+    ddc::DiscreteDomain<Dim0, Dim1> const domain_2d(domain_0, domain_1);
+
+    ddc::Chunk chunk("chunk", domain_2d, ddc::DeviceAllocator<ElementType>());
+    ddc::ChunkSpan const chunk_span = chunk.span_view();
+
+    // Fill the array with 0.12345 in the cells that should be visible and -0.12345 in the one that will be eluded
+    // Check that the output is only aligned on 0.12345
+    auto const subdom_0
+            = domain_0.remove(ddc::DiscreteVector<Dim0>(3), ddc::DiscreteVector<Dim0>(3));
+    auto const subdom_1
+            = domain_1.remove(ddc::DiscreteVector<Dim1>(3), ddc::DiscreteVector<Dim1>(3));
+    ddc::parallel_for_each(
+            domain_2d,
+            KOKKOS_LAMBDA(ddc::DiscreteElement<Dim0, Dim1> const i) {
+                if (subdom_0.contains(ddc::DiscreteElement<Dim0>(i))
+                    || subdom_1.contains(ddc::DiscreteElement<Dim1>(i))) {
+                    chunk_span(i) = -0.12345;
+                } else {
+                    chunk_span(i) = 0.12345;
+                }
+            });
+
+    {
+        std::stringstream ss;
+        print_content(ss, chunk_span);
+        EXPECT_EQ(
+                ss.str(),
+                "[[0.12345 0.12345 0.12345 ... 0.12345 0.12345 0.12345]\n"
+                " [0.12345 0.12345 0.12345 ... 0.12345 0.12345 0.12345]\n"
+                " [0.12345 0.12345 0.12345 ... 0.12345 0.12345 0.12345]\n"
+                " ...\n"
+                " [0.12345 0.12345 0.12345 ... 0.12345 0.12345 0.12345]\n"
+                " [0.12345 0.12345 0.12345 ... 0.12345 0.12345 0.12345]\n"
+                " [0.12345 0.12345 0.12345 ... 0.12345 0.12345 0.12345]]");
+    }
+}
+
+TEST(Print, CheckOutput2dElision)
+{
+    TestPrintCheckoutOutput2dElision<float>();
+    TestPrintCheckoutOutput2dElision<double>();
+}
+
+template <typename ElementType>
+void PrintTestCheckoutOutput3d()
+{
+    unsigned const dim0 = 3;
+    unsigned const dim1 = 3;
+    unsigned const dim2 = 3;
+
+    ddc::DiscreteDomain<Dim0> const domain_0
+            = ddc::init_trivial_bounded_space(ddc::DiscreteVector<Dim0>(dim0));
+    ddc::DiscreteDomain<Dim1> const domain_1
+            = ddc::init_trivial_bounded_space(ddc::DiscreteVector<Dim1>(dim1));
+    ddc::DiscreteDomain<Dim2> const domain_2
+            = ddc::init_trivial_bounded_space(ddc::DiscreteVector<Dim2>(dim2));
+
+    ddc::DiscreteDomain<Dim0, Dim1, Dim2> const domain_3d(domain_0, domain_1, domain_2);
+
+    ddc::Chunk chunk("chunk", domain_3d, ddc::DeviceAllocator<ElementType>());
+    ddc::ChunkSpan const chunk_span = chunk.span_view();
+
+    ddc::parallel_fill(chunk_span, 0.12345);
+
+    {
+        std::stringstream ss;
+        print_content(ss, chunk_span);
+        EXPECT_EQ(
+                ss.str(),
+                "[[[0.12345 0.12345 0.12345]\n"
+                "  [0.12345 0.12345 0.12345]\n"
+                "  [0.12345 0.12345 0.12345]]\n"
+                "\n"
+                " [[0.12345 0.12345 0.12345]\n"
+                "  [0.12345 0.12345 0.12345]\n"
+                "  [0.12345 0.12345 0.12345]]\n"
+                "\n"
+                " [[0.12345 0.12345 0.12345]\n"
+                "  [0.12345 0.12345 0.12345]\n"
+                "  [0.12345 0.12345 0.12345]]]");
+    }
+}
+
+TEST(Print, CheckOutput3d)
+{
+    PrintTestCheckoutOutput3d<float>();
+    PrintTestCheckoutOutput3d<double>();
+}
+
+#if defined(KOKKOS_COMPILER_GNU) || defined(KOKKOS_COMPILER_CLANG)
+void PrintTestMetadata()
+{
+    using ElementType = double;
+
+    unsigned const dim0 = 5;
+    unsigned const dim1 = 5;
+
+    ddc::DiscreteDomain<Dim0> const domain_0
+            = ddc::init_trivial_bounded_space(ddc::DiscreteVector<Dim0>(dim0));
+    ddc::DiscreteDomain<Dim1> const domain_1
+            = ddc::init_trivial_bounded_space(ddc::DiscreteVector<Dim1>(dim1));
+
+    ddc::DiscreteDomain<Dim0, Dim1> const domain_2d(domain_0, domain_1);
+
+    ddc::Chunk chunk("chunk", domain_2d, ddc::DeviceAllocator<ElementType>());
+    ddc::ChunkSpan const chunk_span = chunk.span_view();
+
+    {
+        std::stringstream ss;
+        print_type_info(ss, chunk_span);
+        EXPECT_THAT(
+                ss.str(),
+                testing::MatchesRegex(
+                        "anonymous_namespace_workaround_print_cpp::Dim0\\(5\\)×"
+                        "anonymous_namespace_workaround_print_cpp::Dim1\\(5\\)\n"
+                        "ddc::ChunkSpan<double, ddc::DiscreteDomain"
+                        "<anonymous_namespace_workaround_print_cpp::Dim0,"
+                        " anonymous_namespace_workaround_print_cpp::Dim1>"
+                        ", Kokkos::layout_.+, Kokkos::.+Space>\n"));
+    }
+}
+
+TEST(Print, CheckMetadata)
+{
+    PrintTestMetadata();
+}
+#endif

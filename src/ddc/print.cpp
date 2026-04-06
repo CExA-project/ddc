837f66ce1abc06ef313b9bf6eb34e2bf01b4fc0f
PaulGannay <paul.gannay@cea.fr>
2026-04-06 09:45:54 +0200

    Adds a way to control chunkspan print formating (#1106)
    * Adds a way to control chunkspan print formating
    
    * Apply suggestions from code review
    
    Co-authored-by: Thomas Padioleau <thomas.padioleau@cea.fr>
    
    * Add tests to improve coverage, apply review suggestion
    
    Signed-off-by: Paul Gannay <paul.gannay@cea.fr>
    
    * Spell check
    
    Signed-off-by: Paul Gannay <paul.gannay@cea.fr>
    
    * Add full_print function, use struct for option + linter
    
     - full_print function to always print full array ignoring options
     - use struct to set the options in order to use designated initializers
     - use scoped_lock instead of lock_guard to follow linter recommendation
    
    Signed-off-by: Paul Gannay <paul.gannay@cea.fr>
    
    * fix linter remarks
    
    Signed-off-by: Paul Gannay <paul.gannay@cea.fr>
    
    * Format files
    
    * Minor linting
    
    * Move functions to source file
    
    * Fix missing parenthesis
    
    * Workaround bug with nvcc
    
    ---------
    
    Signed-off-by: Paul Gannay <paul.gannay@cea.fr>
    Co-authored-by: Thomas Padioleau <thomas.padioleau@cea.fr>

--- src/ddc/print.cpp
+++ src/ddc/print.cpp
@@ -3,7 +3,9 @@
 // SPDX-License-Identifier: MIT
 
 #include <cstdlib>
+#include <iostream>
 #include <memory>
+#include <mutex>
 #include <ostream>
 
 #include <Kokkos_Macros.hpp>
@@ -15,7 +17,38 @@
 #    include <cxxabi.h>
 #endif
 
-namespace ddc::detail {
+namespace ddc {
+
+// Workaround for nvcc, it should be `= default` but this creates a missing symbol at link stage.
+bool PrinterOptions::operator==(PrinterOptions const& rhs) const noexcept
+{
+    return threshold == rhs.threshold && edgeitems == rhs.edgeitems;
+}
+
+namespace detail {
+
+ChunkPrinter::~ChunkPrinter() = default;
+
+ChunkPrinter::ChunkPrinter() = default;
+
+void ChunkPrinter::saveformat(std::ostream& os)
+{
+    m_ss.copyfmt(os);
+}
+
+std::ostream& ChunkPrinter::align(std::ostream& os, int const level)
+{
+    for (int i = 0; i <= level; ++i) {
+        os << ' ';
+    }
+    return os;
+}
+
+ChunkPrinter& ChunkPrinter::get_instance()
+{
+    static ChunkPrinter instance;
+    return instance;
+}
 
 void print_demangled_type_name(std::ostream& os, char const* const mangled_name)
 {
@@ -25,11 +58,12 @@ void print_demangled_type_name(std::ostream& os, char const* const mangled_name)
     std::unique_ptr<char, decltype(std::free)*> const
             demangled_name(abi::__cxa_demangle(mangled_name, nullptr, nullptr, &status), std::free);
     if (status != 0) {
-        os << "Error demangling dimension name: " << status;
-        return;
+        std::cerr << "Error demangling dimension name: " << status << '\n' << std::flush;
+        os << mangled_name;
+    } else {
+        os << demangled_name.get();
     }
 
-    os << demangled_name.get();
 #else
     os << mangled_name;
 #endif
@@ -61,4 +95,33 @@ void print_dim_name(
     }
 }
 
-} // namespace ddc::detail
+} // namespace detail
+
+PrinterOptions set_print_options(PrinterOptions const options)
+{
+    ddc::detail::ChunkPrinter& printer = ddc::detail::ChunkPrinter::get_instance();
+
+    PrinterOptions old_options = printer.m_options;
+
+    // Ensure options are not modified while an other thread is printing
+    std::scoped_lock const lock(printer.m_global_lock);
+
+    // Ensure that m_edgeitems < (m_threshold / 2) stays true.
+    if (options.edgeitems < options.threshold / 2) {
+        printer.m_options = options;
+    } else {
+        std::cerr << "DDC Printer: invalid values " << options.edgeitems << " for edgeitems and "
+                  << options.threshold << " for threshold have been ignored\n"
+                  << "threshold needs to be at least twice as big as edgeitems\n";
+    }
+
+    return old_options;
+}
+
+PrinterOptions get_print_options()
+{
+    ddc::detail::ChunkPrinter const& printer = ddc::detail::ChunkPrinter::get_instance();
+    return printer.m_options;
+}
+
+} // namespace ddc

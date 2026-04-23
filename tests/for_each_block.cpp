ab462ca0cbae4cd9c3845d59d764f903319b8e77
Thomas Padioleau <thomas.padioleau@cea.fr>
2026-04-23 11:04:26 +0200

    ND implem

--- tests/for_each_block.cpp
+++ tests/for_each_block.cpp
@@ -2,8 +2,6 @@
 //
 // SPDX-License-Identifier: MIT
 
-#include <list>
-
 #include <ddc/ddc.hpp>
 
 #include <gtest/gtest.h>
@@ -28,6 +26,29 @@ void host_for_each_block(
     }
 }
 
+template <class Support, std::size_t N, class Functor, class... DDoms1d>
+void host_for_each_block(
+        Support const& domain,
+        std::array<DiscreteVectorElement, N> const& nb_blocks,
+        Functor const& f,
+        DDoms1d const&... ddoms) noexcept
+{
+    static constexpr std::size_t I = sizeof...(DDoms1d);
+    if constexpr (I == N) {
+        f(Support(ddoms...));
+    } else {
+        using DDim = ddc::type_seq_element_t<I, ddc::to_type_seq_t<Support>>;
+        std::size_t const block = domain.template extent<DDim>() / nb_blocks[I];
+        std::size_t const rem = domain.template extent<DDim>() - nb_blocks[I] * block;
+        DiscreteElement<DDim> front(domain.front());
+        for (std::size_t ib = 0; ib < nb_blocks[I]; ++ib) {
+            DiscreteVector<DDim> const size(block + (ib < rem ? 1 : 0));
+            host_for_each_block(domain, nb_blocks, f, ddoms..., DiscreteDomain<DDim>(front, size));
+            front += size;
+        }
+    }
+}
+
 } // namespace ddc
 
 inline namespace anonymous_namespace_workaround_for_each_block_cpp {
@@ -39,25 +60,58 @@ using DElemX = ddc::DiscreteElement<DDimX>;
 using DVectX = ddc::DiscreteVector<DDimX>;
 using DDomX = ddc::DiscreteDomain<DDimX>;
 
+struct DDimY
+{
+};
+using DElemY = ddc::DiscreteElement<DDimY>;
+using DVectY = ddc::DiscreteVector<DDimY>;
+using DDomY = ddc::DiscreteDomain<DDimY>;
+
+using DElemXY = ddc::DiscreteElement<DDimX, DDimY>;
+using DVectXY = ddc::DiscreteVector<DDimX, DDimY>;
+using DDomXY = ddc::DiscreteDomain<DDimX, DDimY>;
+
 DElemX constexpr lbound_x = ddc::init_trivial_half_bounded_space<DDimX>();
 DVectX constexpr nelems_x(10);
+
+DElemY constexpr lbound_y = ddc::init_trivial_half_bounded_space<DDimY>();
+DVectY constexpr nelems_y(12);
+
+DElemXY constexpr lbound_x_y(lbound_x, lbound_y);
+DVectXY constexpr nelems_x_y(nelems_x, nelems_y);
 
 } // namespace anonymous_namespace_workaround_for_each_block_cpp
 
 TEST(ForEachSerialBlockHost, OneDimension)
 {
     DDomX const dom(lbound_x, nelems_x);
-    std::vector<int> storage(dom.size(), 0);
-    ddc::ChunkSpan<int, DDomX> const view(storage.data(), dom);
+    ddc::Chunk elems_count("count", dom, ddc::KokkosAllocator<int, Kokkos::HostSpace>());
 
-    std::list<DElemX> list_elems_ref;
-    ddc::host_for_each(dom, [&](DElemX ix) { list_elems_ref.emplace_back(ix); });
+    for (ddc::DiscreteVectorElement const nb_blocks : {1, 2, 10, 15}) {
+        ddc::parallel_fill(elems_count, 0);
+        ddc::host_for_each_block(
+                dom,
+                std::array<ddc::DiscreteVectorElement, 1> {nb_blocks},
+                [&](DDomX const domx) {
+                    ddc::host_for_each(domx, [&](DElemX ix) { elems_count(ix)++; });
+                });
+        ddc::host_for_each(dom, [&](DElemX ix) { EXPECT_EQ(elems_count(ix), 1); });
+    }
+}
 
-    for (std::size_t const block_size : {1, 2, 10, 15}) {
-        std::list<DElemX> list_elems;
-        ddc::host_for_each_block(dom, block_size, [&](DDomX const domx) {
-            ddc::host_for_each(domx, [&](DElemX ix) { list_elems.emplace_back(ix); });
-        });
-        EXPECT_EQ(list_elems_ref, list_elems);
+TEST(ForEachSerialBlockHost, TwoDimensions)
+{
+    DDomXY const dom(lbound_x_y, nelems_x_y);
+    ddc::Chunk elems_count("count", dom, ddc::KokkosAllocator<int, Kokkos::HostSpace>());
+
+    for (ddc::DiscreteVectorElement const nb_blocks : {1, 2, 10, 15}) {
+        ddc::parallel_fill(elems_count, 0);
+        ddc::host_for_each_block(
+                dom,
+                std::array<ddc::DiscreteVectorElement, 2> {nb_blocks, nb_blocks},
+                [&](DDomXY const domxy) {
+                    ddc::host_for_each(domxy, [&](DElemXY ixy) { elems_count(ixy)++; });
+                });
+        ddc::host_for_each(dom, [&](DElemXY ixy) { EXPECT_EQ(elems_count(ixy), 1); });
     }
 }

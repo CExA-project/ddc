9cbb567056e8f77cea43d866890a6aa593dfd4d1
Thomas Padioleau <thomas.padioleau@cea.fr>
2026-06-30 09:34:12 +0200

    Update tests/type_seq.cpp

--- tests/type_seq.cpp
+++ tests/type_seq.cpp
@@ -48,8 +48,7 @@ TEST(TypeSeqTest, Rank)
     EXPECT_EQ((ddc::type_seq_rank_v<T0, A>), 0);
     EXPECT_EQ((ddc::type_seq_rank_v<T1, A>), 1);
     EXPECT_EQ((ddc::type_seq_rank_v<T2, A>), 2);
-    constexpr std::size_t max_val = std::numeric_limits<std::size_t>::max();
-    EXPECT_EQ((ddc::type_seq_rank_v<T3, A>), max_val);
+    EXPECT_EQ((ddc::type_seq_rank_v<T3, A>), (std::numeric_limits<std::size_t>::max()));
 }
 
 TEST(TypeSeqTest, Element)

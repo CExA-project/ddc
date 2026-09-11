7a43538b2cd843a0c9683f2717019f21f4f6aa7e
Thomas Padioleau <thomas.padioleau@cea.fr>
2026-09-11 07:32:00 +0200

    Update doxygen of PeriodicExtrapolationRule

--- src/ddc/kernels/splines/periodic_extrapolation_rule.hpp
+++ src/ddc/kernels/splines/periodic_extrapolation_rule.hpp
@@ -9,9 +9,20 @@
 
 namespace ddc {
 
+/**
+ * @brief A functor to represent periodic extrapolation in a 1D spline evaluator.
+ *
+ * This rule is handled by the spline evaluator itself and therefore this
+ * functor should never be invoked.
+ */
 template <class DimI>
 struct PeriodicExtrapolationRule
 {
+    /**
+     * @brief This function should never be called.
+     *
+     * @return Undefined.
+     */
     template <class CoordType, class ChunkSpan>
     KOKKOS_FUNCTION double operator()(CoordType, ChunkSpan) const
     {

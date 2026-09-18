6fe9cff39f4ffbfdaad914e40218cedbc5c87b73
Emily Bourne <emily.bourne@epfl.ch>
2026-09-18 14:28:52 +0200

    Flag deprecation

--- src/ddc/kernels/splines/constant_extrapolation_rule.hpp
+++ src/ddc/kernels/splines/constant_extrapolation_rule.hpp
@@ -20,7 +20,7 @@ namespace ddc {
  * extrapolation on the edge.
  */
 #if DDC_BUILD_DEPRECATED_CODE()
-template <class DimI, class... DimNI>
+template <class DimI, [[deprecated]] class... DimNI>
 #else
 template <class DimI>
 #endif
